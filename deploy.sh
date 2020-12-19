fd4150bbc314ea70541820e8067cbfd96b0b9b99
Chef Nobu <m4z3y@yandex.com>
2020-12-18 19:53:54 -0700

    Update deploy file

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
-yarn build;
+# yarn build;
 
-aws s3 sync build/ s3://dex.itchiro.com/ --profile itchiro;
+# aws s3 sync build/ s3:// --profile [profile];
 
-# aws cloudfront create-invalidation --distribution-id E2T0SKBSRO9AGX --paths "/";
+# aws cloudfront create-invalidation --distribution-id E2T0SKBSRO9AGX --paths "/" --profile [profile];
