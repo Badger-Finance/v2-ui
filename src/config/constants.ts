57b0475d450d0b2c699d87e92dec4688df93db8e
Jon Stevens <latchkey@gmail.com>
2021-01-17 11:27:46 -0800

    format round 2

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -1,4 +1,4 @@
-import BigNumber from 'bignumber.js';
+import importedErc20 from '../config/system/abis/ERC20.json';
 
 export const INFURA_KEY = '77a0f6647eb04f5ca1409bba62ae9128';
 export const APP_URL = 'https://app.badger.finance/';
@@ -7,8 +7,8 @@ export const RPC_URL = 'https://mainnet.infura.io/v3/32d16e34f8af476e9ef63b34ba2
 export const APP_NAME = 'badgerDAO';
 
 export const WBTC_ADDRESS = '0x2260fac5e5542a773aa44fbcfedf7c193bc2c599';
-export const ERC20 = require('config/system/abis/ERC20.json');
+export const ERC20 = importedErc20;
 export const START_BLOCK = 11381216;
 export const START_TIME = new Date('Dec 03 2020 06:11:35 PM UTC');
 export const EMPTY_DATA = '0x';
-export const MIN_ETH_BALANCE = new BigNumber(0.01 * 1e18);
+// export const MIN_ETH_BALANCE = new BigNumber(0.01 * 1e18);
