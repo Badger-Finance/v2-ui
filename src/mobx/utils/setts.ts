553bbb07a7989c878444fda7d00b730755c4eba6
chainergar <garmoniart@gmail.com>
2021-02-02 12:55:37 -0500

    Implement sett vision api

---
+++ src/mobx/utils/setts.ts
@@ -0,0 +1,82 @@
+export const setts = [
+	{
+		title: 'Badger',
+		asset: 'badger',
+		symbol: 'badger',
+		address: '0x19D97D8fA813EE2f51aD4B4e04EA08bAf4DFfC28',
+		position: 2,
+	},
+	{
+		title: 'Sushi BADGER-WBTC',
+		asset: 'slp-badger-wbtc',
+		symbol: 'wbtcBadgerSLP',
+		address: '0x1862A18181346EBd9EdAf800804f89190DeF24a5',
+		position: 4,
+	},
+	{
+		title: 'Sushi WBTC-ETH',
+		asset: 'slp-wbtc-eth',
+		symbol: 'wbtcWethSLP',
+		address: '0x758A43EE2BFf8230eeb784879CdcFF4828F2544D',
+		position: 5,
+	},
+	{
+		title: 'Uniswap BADGER-WBTC',
+		asset: 'badger-wbtc',
+		symbol: 'wbtcBadgerUNI-V2',
+		address: '0x235c9e24D3FB2FAFd58a2E49D454Fdcd2DBf7FF1',
+		position: 7,
+	},
+	{
+		title: 'Harvest renBTC',
+		asset: 'hrenbtccrv',
+		symbol: 'crvRenWBTC',
+		address: '0xAf5A1DECfa95BAF63E0084a35c62592B774A2A87',
+		position: 8,
+	},
+	{
+		title: 'tBTC',
+		asset: 'tbtccrv',
+		symbol: 'tbtc/sbtcCrv',
+		address: '0xb9D076fDe463dbc9f915E5392F807315Bf940334',
+		position: 9,
+	},
+	{
+		title: 'sBTC',
+		asset: 'sbtccrv',
+		symbol: 'crvRenWSBTC',
+		address: '0xd04c48A53c111300aD41190D63681ed3dAd998eC',
+		position: 10,
+	},
+	{
+		title: 'renBTC',
+		asset: 'renbtccrv',
+		symbol: 'crvRenWBTC',
+		address: '0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545',
+		position: 11,
+	},
+];
+
+export const diggSetts = [
+	{
+		title: 'Digg',
+		asset: 'digg',
+		symbol: 'digg',
+		address: '0x7e7e112a68d8d2e221e11047a72ffc1065c38e1a',
+		position: 1,
+	},
+	{
+		title: 'Sushi DIGG-WBTC',
+		asset: 'slp-digg-wbtc',
+		symbol: 'wbtcDiggSLP',
+		address: '0x1862A18181346EBd9EdAf800804f89190DeF24a5',
+		position: 3,
+	},
+	{
+		title: 'Uniswap DIGG-WBTC',
+		asset: 'digg-wbtc',
+		symbol: 'wbtcDiggUNI-V2',
+		address: '0xC17078FDd324CC473F8175Dc5290fae5f2E84714',
+		position: 6,
+	},
+];
