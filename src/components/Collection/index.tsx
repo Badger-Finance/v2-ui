ecfa4957bfcded8eea3f164846266ebc70e10b0d
chainergar <garmoniart@gmail.com>
2021-02-02 12:55:37 -0500

    Implement sett vision api

--- src/components/Collection/index.tsx
+++ src/components/Collection/index.tsx
@@ -16,7 +16,6 @@ import {
 	List,
 	ListItem,
 	ListItemText,
-	ListItemSecondaryAction,
 	Tooltip,
 } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
@@ -26,6 +25,7 @@ import { Loader } from '../Loader';
 import { SettList } from './SettList';
 import { CLAIMS_SYMBOLS } from 'config/constants';
 import { formatPrice } from 'mobx/reducers/statsReducers';
+import { formatUsd } from 'mobx/utils/api';
 import { inCurrency } from '../../mobx/utils/helpers';
 import useInterval from '@use-it/interval';
 import Hero from 'components/Common/Hero';
@@ -111,7 +111,7 @@ export const Collection = observer(() => {
 
 	const {
 		wallet: { connectedAddress, isCached },
-		contracts: { tokens },
+		contracts: { tokens, assets, badger },
 		rewards: { claimGeysers, badgerTree },
 		uiState: {
 			stats,
@@ -147,7 +147,16 @@ export const Collection = observer(() => {
 	};
 
 	const rewards = _.compact(availableRewards());
+<<<<<<< HEAD
 	//
+=======
+	const tvl = assets.totalValue ? formatUsd(assets.totalValue) : '$0.00';
+	const badgerPrice =
+		badger && badger.market_data && badger.market_data.current_price
+			? formatUsd(badger.market_data.current_price.usd)
+			: '$0.00';
+
+>>>>>>> d6194f2... Implement setts vision api
 	return (
 		<>
 			<Container className={classes.root}>
@@ -213,7 +222,7 @@ export const Collection = observer(() => {
 							<Typography variant="subtitle1" color="textPrimary">
 								TVL
 							</Typography>
-							<Typography variant="h5">{formatPrice(stats.stats.tvl, currency)}</Typography>
+							<Typography variant="h5">{tvl}</Typography>
 						</Paper>
 					</Grid>
 					{!!connectedAddress && (
@@ -232,7 +241,7 @@ export const Collection = observer(() => {
 							<Typography variant="subtitle1" color="textPrimary">
 								Badger Price
 							</Typography>
-							<Typography variant="h5">{formatPrice(stats.stats.badger, currency)}</Typography>
+							<Typography variant="h5">{badgerPrice}</Typography>
 						</Paper>
 					</Grid>
 
