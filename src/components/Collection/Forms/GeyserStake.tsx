cbb5ea094e94e5f7e7b8d35d2a7a10d661239c67
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2021-02-17 10:10:57 -0500

    Linting - Removed 'empty patterns' at store deconstructing
    The `router` and `uistate` location helpers were removed from the store's deconstructing since they were not instantiating any variables and, hence, were not in use. This satisfied the 'no-empty-pattern' eslint rule.

--- src/components/Collection/Forms/GeyserStake.tsx
+++ src/components/Collection/Forms/GeyserStake.tsx
@@ -1,6 +1,5 @@
 import React, { useContext } from 'react';
 import { observer } from 'mobx-react-lite';
-
 import { StoreContext } from '../../../mobx/store-context';
 import { Button, DialogContent, TextField, DialogActions, ButtonGroup } from '@material-ui/core';
 import { Typography } from '@material-ui/core';
@@ -27,9 +26,7 @@ export const GeyserStake = observer((props: any) => {
 	const { register, handleSubmit, watch, setValue } = useForm({ mode: 'all' });
 
 	const {
-		router: {},
 		wallet: { connectedAddress },
-		uiState: {},
 	} = store;
 
 	const percentageOfBalance = (percent: number) => {
