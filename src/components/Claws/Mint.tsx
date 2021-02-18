2258743eb070503a72a4f4e0f7b89131ebbee955
Elio <eliobricenov@gmail.com>
2021-02-17 23:34:25 -0400

    feat: created clawparams component

--- src/components/Claws/Mint.tsx
+++ src/components/Claws/Mint.tsx
@@ -1,73 +1,19 @@
-import React, { useState } from 'react';
-import {
-	createStyles,
-	FormControl,
-	Grid,
-	InputBase,
-	makeStyles,
-	MenuItem,
-	Select,
-	withStyles,
-	Typography,
-	Button,
-	ButtonGroup,
-} from '@material-ui/core';
-import watch from 'material-ui/svg-icons/hardware/watch';
+import React, { FC } from 'react';
+import { Container, Grid, Box } from '@material-ui/core';
+import ClawParams from './ClawParams';
 
-const useStyles = makeStyles((theme) => ({
-	margin: {
-		margin: theme.spacing(1),
-	},
-}));
-
-const BorderlessInput = withStyles((theme) =>
-	createStyles({
-		root: {
-			'label + &': {
-				marginTop: theme.spacing(4),
-			},
-		},
-		input: {
-			borderRadius: 4,
-			fontSize: 16,
-			fontWeight: 'bold',
-		},
-	}),
-)(InputBase);
-
-export const Mint = () => {
-	const classes = useStyles();
-	const [collateral, setCollateral] = useState('wbtcWethSLP');
-
-	const handleChange = (event: React.ChangeEvent<{ value: unknown }>) => {
-		setCollateral(event.target.value as string);
-	};
-
+export const Mint: FC = () => {
 	return (
-		<Grid container>
-			<Grid item>
-				<FormControl className={classes.margin}>
-					<Select value={collateral} onChange={handleChange} input={<BorderlessInput />}>
-						<MenuItem value="wbtcWethSLP">wbtcWethSLP</MenuItem>
-						<MenuItem value="wbtcWethSLP2">wbtcWethSLP2</MenuItem>
-						<MenuItem value="wbtcWethSLP3">wbtcWethSL3</MenuItem>
-					</Select>
-				</FormControl>
-			</Grid>
-			<Grid item>
-				<Typography variant="subtitle1" color="textSecondary">
-					0.00
-				</Typography>
-			</Grid>
-			<Grid item>
-				<ButtonGroup size="small" aria-label="small outlined button group">
-					{[25, 50, 75, 100].map((amount: number, index: number) => (
-						<Button key={`button_${amount}_${index}`} variant="outlined" style={{ color: 'white' }}>
-							{amount}%
-						</Button>
-					))}
-				</ButtonGroup>
-			</Grid>
-		</Grid>
+		<Box clone px={3}>
+			<Container>
+				<ClawParams
+					tokens={[
+						{ name: 'wbtcWethSLP', balance: '100' },
+						{ name: 'wbtcWethSLP2', balance: '200' },
+						{ name: 'wbtcWethSLP3', balance: '300' },
+					]}
+				/>
+			</Container>
+		</Box>
 	);
 };
