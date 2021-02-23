98133f38e6ae2d67f97a949d7aff572db75fb058
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2021-02-22 19:46:07 -0500

    QA Linting - Removed commented out values

--- src/mobx/reducers/index.ts
+++ src/mobx/reducers/index.ts
@@ -113,11 +113,7 @@ class UiState {
 	reduceRebase = action(() => {
 		const { tokens } = this.store.contracts;
 		if (!!this.store.rebase.rebase && !!tokens[WBTC_ADDRESS])
-			this.rebaseStats = reduceRebase(
-				this.store.rebase.rebase,
-				tokens[WBTC_ADDRESS],
-				/* tokens[digg_system.uFragments], // Not in use within method */
-			);
+			this.rebaseStats = reduceRebase(this.store.rebase.rebase, tokens[WBTC_ADDRESS]);
 	});
 
 	setGasPrice = action((gasPrice: string) => {
