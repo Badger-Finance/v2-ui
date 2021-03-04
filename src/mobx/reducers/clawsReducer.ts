1fb9f676935068062ee2e9091209301f867ea221
Elio <eliobricenov@gmail.com>
2021-03-04 00:48:46 -0400

    add hex to bignumber parsers

--- src/mobx/reducers/clawsReducer.ts
+++ src/mobx/reducers/clawsReducer.ts
@@ -1,3 +1,4 @@
+import BigNumber from 'bignumber.js';
 import { invert as _invert, valuesIn as _valuesIn } from 'lodash';
 import { ClawStore, SponsorData, SyntheticData } from 'mobx/stores/clawStore';
 import { symbols as TOKEN_SYMBOLS } from 'config/system/tokens';
@@ -18,12 +19,12 @@ export function reduceCollaterals({ syntheticsData }: ClawStore): Map<string, st
 
 // [EMPS_ADDRESS: string] => [SYNTHETIC_DATA: SyntheticData]
 export function reduceSyntheticsData({ syntheticsData }: ClawStore): Map<string, SyntheticData> {
-	return syntheticsData.reduce(indexByEmpAddress, new Map());
+	return syntheticsData.map(parseSyntheticHexToBigNumber).reduce(indexByEmpAddress, new Map());
 }
 
 // [EMPS_ADDRESS: string]  =>[SPONSOR_DATA: SponsorData]
 export function reduceSponsorData({ sponsorInformation }: ClawStore): Map<string, SponsorData> {
-	return sponsorInformation.reduce(indexByEmpAddress, new Map());
+	return sponsorInformation.map(parseSponsorsHexToBigNumber).reduce(indexByEmpAddress, new Map());
 }
 
 // [COLLATERAL_ADDRESS: string] => [ECLAW: [ECLAW_ADDRESS: string] => [ECLAW_NAME: string]]
@@ -31,7 +32,7 @@ export function reduceEclawByCollateral({ collaterals, syntheticsData }: ClawSto
 	return Array.from(collaterals).reduce(indexEclawsByCollateralAddress(syntheticsData), new Map());
 }
 
-// helper functions
+// reducer's helper functions
 
 function indexByCollateralAddress(collaterals: Map<string, string>, { collateralCurrency }: SyntheticData) {
 	return collaterals.set(
@@ -64,4 +65,84 @@ function hasTokenInformation({ collateralCurrency }: SyntheticData) {
 	return Object.keys(TOKEN_SYMBOLS)
 		.map((key) => key.toLocaleLowerCase())
 		.includes(collateralCurrency.toLocaleLowerCase());
+}
+
+// hex to big number parsers
+
+function parseSponsorsHexToBigNumber({ liquidations, position, pendingWithdrawal }: SponsorData): SponsorData {
+	return {
+		pendingWithdrawal,
+		liquidations: liquidations.map(parseLiquidationHexToBigNumber),
+		position: parsePositionHexToBigNumber(position),
+	};
+}
+
+function parseSyntheticHexToBigNumber(data: SyntheticData): SyntheticData {
+	const {
+		globalCollateralizationRatio,
+		totalPositionCollateral,
+		totalTokensOutstanding,
+		expirationTimestamp,
+		cumulativeFeeMultiplier,
+		minSponsorTokens,
+		withdrawalLiveness,
+		liquidationLiveness,
+		...skipParse
+	} = data;
+
+	return {
+		...skipParse,
+		globalCollateralizationRatio: new BigNumber((globalCollateralizationRatio as any).hex),
+		totalPositionCollateral: new BigNumber((totalPositionCollateral as any).hex),
+		totalTokensOutstanding: new BigNumber((totalTokensOutstanding as any).hex),
+		expirationTimestamp: new BigNumber((expirationTimestamp as any).hex),
+		cumulativeFeeMultiplier: new BigNumber((cumulativeFeeMultiplier as any).hex),
+		minSponsorTokens: new BigNumber((minSponsorTokens as any).hex),
+		withdrawalLiveness: new BigNumber((withdrawalLiveness as any).hex),
+		liquidationLiveness: new BigNumber((liquidationLiveness as any).hex),
+	};
+}
+
+function parseLiquidationHexToBigNumber(data: SponsorData['liquidations'][0]): SponsorData['liquidations'][0] {
+	const {
+		state,
+		liquidatedCollateral,
+		tokensOutstanding,
+		lockedCollateral,
+		liquidationTime,
+		rawUnitCollateral,
+		settlementPrice,
+		finalFee,
+		...skipParse
+	} = data;
+
+	return {
+		...skipParse,
+		state: new BigNumber((state as any).hex),
+		liquidatedCollateral: new BigNumber((liquidatedCollateral as any).hex),
+		tokensOutstanding: new BigNumber((tokensOutstanding as any).hex),
+		lockedCollateral: new BigNumber((lockedCollateral as any).hex),
+		liquidationTime: new BigNumber((liquidationTime as any).hex),
+		rawUnitCollateral: new BigNumber((rawUnitCollateral as any).hex),
+		settlementPrice: new BigNumber((settlementPrice as any).hex),
+		finalFee: new BigNumber((finalFee as any).hex),
+	};
+}
+
+function parsePositionHexToBigNumber(data: SponsorData['position']): SponsorData['position'] {
+	const {
+		tokensOutstanding,
+		withdrawalRequestPassTimestamp,
+		withdrawalRequestAmount,
+		rawCollateral,
+		transferPositionRequestPassTimestamp,
+	} = data;
+
+	return {
+		tokensOutstanding: new BigNumber((tokensOutstanding as any).hex),
+		withdrawalRequestPassTimestamp: new BigNumber((withdrawalRequestPassTimestamp as any).hex),
+		withdrawalRequestAmount: new BigNumber((withdrawalRequestAmount as any).hex),
+		rawCollateral: new BigNumber((rawCollateral as any).hex),
+		transferPositionRequestPassTimestamp: new BigNumber((transferPositionRequestPassTimestamp as any).hex),
+	};
 }
