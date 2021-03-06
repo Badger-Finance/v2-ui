262bb73a5fa0d45b16af8a73ba3eb365d57da1c8
Elio <eliobricenov@gmail.com>
2021-03-06 00:31:28 -0400

    export wallet connection function and create a new connet to wallet button to be used in claw components

---
+++ src/components/Claws/ConnectWalletButton.tsx
@@ -0,0 +1,22 @@
+import { Button, makeStyles } from '@material-ui/core';
+import { connectToWallet } from 'components/Sidebar/Wallet';
+import { StoreContext } from 'mobx/store-context';
+import React, { useContext } from 'react';
+import { useMainStyles } from './index';
+
+export function ConnectWalletButton() {
+	const classes = useMainStyles();
+    const store = useContext(StoreContext);
+
+	return (
+		<Button
+			color="primary"
+			onClick={() => connectToWallet(store)}
+			variant="contained"
+			size="large"
+			className={classes.button}
+		>
+			Connect Wallet
+		</Button>
+	);
+}
