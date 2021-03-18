03af1913b56806ac0de0a07ced196c6214e992e2
elio <eliobricenov@gmail.com>
2021-03-18 02:27:27 -0400

    feat: add honeypot store for fetching pool balance and nft information

--- src/mobx/store.ts
+++ src/mobx/store.ts
@@ -9,6 +9,7 @@ import IbBTCStore from './stores/ibBTCStore';
 import TransactionsStore from './stores/transactionsStore';
 import SettStoreV2 from './stores/settStoreV2';
 import { NETWORK_LIST } from '../config/constants';
+import HoneyPotStore from './stores/honeyPotStore';
 
 export class RootStore {
 	public router: RouterStore<RootStore>;
@@ -21,6 +22,7 @@ export class RootStore {
 	public ibBTCStore: IbBTCStore;
 	public setts: SettStoreV2;
 	public transactions: TransactionsStore;
+	public honeyPot: HoneyPotStore;
 
 	constructor() {
 		this.router = new RouterStore<RootStore>(this);
@@ -31,6 +33,7 @@ export class RootStore {
 		this.rewards = new RewardsStore(this);
 		this.uiState = new UiState(this);
 		this.ibBTCStore = new IbBTCStore(this);
+		this.honeyPot = new HoneyPotStore(this);
 		// RenVM transactions store.
 		this.transactions = new TransactionsStore(this);
 		this.setts = new SettStoreV2(this);
