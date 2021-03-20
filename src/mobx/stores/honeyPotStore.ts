cd57a3b403348c380ff8802bbc632bc349161464
elio <eliobricenov@gmail.com>
2021-03-19 22:00:49 -0400

    wip: add initial functionality for redeeming NFT

--- src/mobx/stores/honeyPotStore.ts
+++ src/mobx/stores/honeyPotStore.ts
@@ -11,6 +11,9 @@ import mainnet from 'config/deployments/mainnet.json';
 import { abi as scarcityPoolABI } from 'config/system/abis/BadgerScarcityPool.json';
 import { abi as memeLtdABI } from 'config/system/abis/MemeLtd.json';
 import { reduceNextGlobalRedemptionRate } from 'mobx/reducers/honeypotReducer';
+import { estimateAndSend } from 'mobx/utils/web3';
+import { PromiEvent } from 'web3-core';
+import { Contract } from 'web3-eth-contract';
 
 export interface NFT {
 	tokenId: string;
@@ -25,6 +28,7 @@ export class HoneyPotStore {
 	poolBalance?: BigNumber;
 	nextRedemptionRate?: BigNumber;
 	nfts?: NFT[];
+	nftBeingRedeemed?: string;
 	loadingPoolBalance = false;
 	loadingNfts = false;
 
@@ -36,6 +40,7 @@ export class HoneyPotStore {
 			loadingPoolBalance: this.loadingPoolBalance,
 			loadingNfts: this.loadingNfts,
 			nextRedemptionRate: this.nextRedemptionRate,
+			nftBeingRedeemed: this.nftBeingRedeemed,
 		});
 
 		observe(this.store.wallet, 'connectedAddress', () => {
@@ -93,18 +98,18 @@ export class HoneyPotStore {
 				}
 			}
 
-			const [balances] = await Promise.all([
-				memeLtd.methods
-					.balanceOfBatch(
-						Array(nfts.length).fill(connectedAddress),
-						nfts.map(({ tokenId }) => tokenId),
-					)
-					.call(),
+			const tokenIds = nfts.map(({ tokenId }) => tokenId);
+			const [balances, { assets }] = await Promise.all([
+				memeLtd.methods.balanceOfBatch(Array(nfts.length).fill(connectedAddress), tokenIds).call(),
+				getNftBatchInformation(tokenIds),
 			]);
 
-			this.nfts = _mergeWith(nfts, balances, (nfts, balance) => ({
-				...nfts,
-				balance,
+			// merge nft core information, balance, name and image
+			this.nfts = nfts.map((nft, index) => ({
+				...nft,
+				balance: balances[index],
+				image: assets[index]?.imagePreviewUrl,
+				name: assets[index]?.name,
 			}));
 
 			this.nextRedemptionRate = reduceNextGlobalRedemptionRate(this);
@@ -114,6 +119,52 @@ export class HoneyPotStore {
 			process.env.NODE_ENV != 'production' && console.error(error);
 		} finally {
 			this.loadingNfts = false;
+		}
+	});
+
+	redeemNFT = action(async (tokenId: string) => {
+		try {
+			const { queueNotification, gasPrice, setTxStatus } = this.store.uiState;
+			const { provider, connectedAddress, gasPrices } = this.store.wallet;
+			if (!connectedAddress) return;
+
+			this.nftBeingRedeemed = tokenId;
+			const web3 = new Web3(provider);
+			const pool = new web3.eth.Contract(scarcityPoolABI as AbiItem[], mainnet.honeypotMeme);
+			const memeLtdAddress = await pool.methods.memeLtd().call();
+			const memeLtd = new web3.eth.Contract(memeLtdABI as AbiItem[], memeLtdAddress);
+
+			const redeem = memeLtd.methods.safeTransferFrom(memeLtdAddress, mainnet.honeypotMeme, tokenId, '1', '0x00');
+
+			queueNotification(`Sign the transaction to redeem your NFT`, 'info');
+
+			estimateAndSend(
+				web3,
+				gasPrices[gasPrice],
+				redeem,
+				connectedAddress,
+				(transaction: PromiEvent<Contract>) => {
+					transaction
+						.on('transactionHash', (hash) => {
+							queueNotification(`Redemption submitted.`, 'info', hash);
+							this.nftBeingRedeemed = undefined;
+						})
+						.on('receipt', () => {
+							queueNotification(`NFT Redeemed.`, 'success');
+							this.fetchNFTS();
+							this.nftBeingRedeemed = undefined;
+						})
+						.catch((error: any) => {
+							queueNotification(error.message, 'error');
+							setTxStatus('error');
+							this.nftBeingRedeemed = undefined;
+						});
+				},
+			);
+		} catch (error) {
+			const message = error?.message || 'There was an error. Please try again later.';
+			this.store.uiState.queueNotification(message, 'error');
+			process.env.NODE_ENV != 'production' && console.error(error);
 		}
 	});
 
