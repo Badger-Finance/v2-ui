c93b9d3f086a9f57ef42cbe94c4557de30428fc1
elio <eliobricenov@gmail.com>
2021-03-25 17:45:42 -0400

    feat: add mute/unmute icons

--- src/components/HoneybadgerDrop/NftStats.tsx
+++ src/components/HoneybadgerDrop/NftStats.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import { Box, Button, CircularProgress, Grid, makeStyles, Paper, Typography } from '@material-ui/core';
+import VolumeOffIcon from '@material-ui/icons/VolumeOff';
+import VolumeUpIcon from '@material-ui/icons/VolumeUp';
 
 interface Props {
 	nftId: string;
@@ -37,7 +39,24 @@ const useStyles = makeStyles((theme) => ({
 	spinner: {
 		color: theme.palette.common.white,
 	},
+	soundIcon: {
+		cursor: 'pointer',
+		position: 'absolute',
+		right: 0,
+		top: -8,
+	},
+	statsContainer: {
+		position: 'relative',
+		padding: theme.spacing(2),
+	},
 }));
+
+const SoundIcon: React.FC<{ muted: boolean; onClick: () => void }> = ({ muted, onClick }) => {
+	const classes = useStyles();
+	const Icon = muted ? VolumeOffIcon : VolumeUpIcon;
+
+	return <Icon className={classes.soundIcon} onClick={onClick} />;
+};
 
 const NftStats: React.FC<Props> = ({
 	nftId,
@@ -51,6 +70,7 @@ const NftStats: React.FC<Props> = ({
 	loading = false,
 	onRedeem,
 }) => {
+	const [muted, setMuted] = React.useState(true);
 	const classes = useStyles();
 
 	return (
@@ -64,66 +84,78 @@ const NftStats: React.FC<Props> = ({
 							autoPlay
 							loop
 							playsInline
-							muted
+							muted={muted}
+							onLoadStart={(e) => {
+								e.currentTarget.volume = 0.19;
+							}}
+							preload="auto"
 						/>
 					</Grid>
 				)}
-				<Grid item xs={12}>
-					<Typography className={classes.textCenter}>{name}</Typography>
-				</Grid>
-				<Grid item container xs={12}>
-					<Grid item container justify="space-between" xs={12}>
-						<Grid item xs={6} className={classes.textStart}>
-							<Typography variant="caption" color="textSecondary">
-								Your Balance
-							</Typography>
-						</Grid>
-						<Grid item xs={6} className={classes.textEnd}>
-							<Typography variant="caption">{balance}</Typography>
-						</Grid>
+				<Grid item container className={classes.statsContainer}>
+					<SoundIcon
+						muted={muted}
+						onClick={() => {
+							setMuted(!muted);
+						}}
+					/>
+					<Grid item xs={12}>
+						<Typography className={classes.textCenter}>{name}</Typography>
 					</Grid>
-					<Grid item container justify="space-between" xs={12}>
-						<Grid item xs={6} className={classes.textStart}>
-							<Typography variant="caption" color="textSecondary">
-								Remaining
-							</Typography>
+					<Grid item container xs={12}>
+						<Grid item container justify="space-between" xs={12}>
+							<Grid item xs={6} className={classes.textStart}>
+								<Typography variant="caption" color="textSecondary">
+									Your Balance
+								</Typography>
+							</Grid>
+							<Grid item xs={6} className={classes.textEnd}>
+								<Typography variant="caption">{balance}</Typography>
+							</Grid>
 						</Grid>
-						<Grid item xs={6} className={classes.textEnd}>
-							<Typography variant="caption">{remaining}</Typography>
+						<Grid item container justify="space-between" xs={12}>
+							<Grid item xs={6} className={classes.textStart}>
+								<Typography variant="caption" color="textSecondary">
+									Remaining
+								</Typography>
+							</Grid>
+							<Grid item xs={6} className={classes.textEnd}>
+								<Typography variant="caption">{remaining}</Typography>
+							</Grid>
 						</Grid>
-					</Grid>
-					<Grid item container justify="space-between" xs={12}>
-						<Grid item xs={6} className={classes.textStart}>
-							<Typography variant="caption" color="textSecondary">
-								Redemption Value (bDIGG)
-							</Typography>
+						<Grid item container justify="space-between" xs={12}>
+							<Grid item xs={6} className={classes.textStart}>
+								<Typography variant="caption" color="textSecondary">
+									Redemption Value (bDIGG)
+								</Typography>
+							</Grid>
+							<Grid item xs={6} className={classes.textEnd}>
+								<Typography variant="caption">{redemptionRateBdigg}</Typography>
+							</Grid>
 						</Grid>
-						<Grid item xs={6} className={classes.textEnd}>
-							<Typography variant="caption">{redemptionRateBdigg}</Typography>
+						<Grid item container justify="space-between" xs={12}>
+							<Grid item xs={6} className={classes.textStart}>
+								<Typography variant="caption" color="textSecondary">
+									Redemption Value ($)
+								</Typography>
+							</Grid>
+							<Grid item xs={6} className={classes.textEnd}>
+								<Typography variant="caption">{redemptionRateUsd}</Typography>
+							</Grid>
 						</Grid>
-					</Grid>
-					<Grid item container justify="space-between" xs={12}>
-						<Grid item xs={6} className={classes.textStart}>
-							<Typography variant="caption" color="textSecondary">
-								Redemption Value ($)
-							</Typography>
+						<Grid item container justify="center" xs={12}>
+							<Box clone mt={2}>
+								<Button
+									disabled={disabled || loading}
+									className={classes.redeemButton}
+									variant="contained"
+									color="primary"
+									onClick={() => onRedeem(nftId)}
+								>
+									{loading ? <CircularProgress className={classes.spinner} size={20} /> : 'Redeem'}
+								</Button>
+							</Box>
 						</Grid>
-						<Grid item xs={6} className={classes.textEnd}>
-							<Typography variant="caption">{redemptionRateUsd}</Typography>
-						</Grid>
-					</Grid>
-					<Grid item container justify="center" xs={12}>
-						<Box clone mt={2}>
-							<Button
-								disabled={disabled || loading}
-								className={classes.redeemButton}
-								variant="contained"
-								color="primary"
-								onClick={() => onRedeem(nftId)}
-							>
-								{loading ? <CircularProgress className={classes.spinner} size={20} /> : 'Redeem'}
-							</Button>
-						</Box>
 					</Grid>
 				</Grid>
 			</Grid>
