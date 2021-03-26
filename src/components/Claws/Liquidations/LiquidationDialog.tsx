d1f1ccf6dc6007e2e0779c5027253def9c217088
Colin Spence <udappia@gmail.com>
2021-03-25 22:57:17 -0600

    Creates initial Liquidation Transaction Dialog

---
+++ src/components/Claws/Liquidations/LiquidationDialog.tsx
@@ -0,0 +1,103 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import {
+	Button,
+	Dialog,
+	DialogActions,
+	DialogContent,
+	DialogTitle,
+	Divider,
+	Grid,
+	Typography,
+} from '@material-ui/core';
+
+export const useMainStyles = makeStyles((theme) => ({
+	transactionRow: {
+		marginTop: theme.spacing(1),
+	},
+	dialogTitle: {
+		borderBottom: theme.palette.common.white,
+	},
+	dialogContent: {
+		paddingBottom: theme.spacing(2),
+		paddingTop: theme.spacing(0),
+	},
+	dialogActions: {
+		paddingBottom: theme.spacing(2),
+		paddingTop: theme.spacing(2),
+		paddingLeft: theme.spacing(3),
+		paddingRight: theme.spacing(3),
+	},
+	mx: {
+		marginRight: theme.spacing(3),
+		marginLeft: theme.spacing(3),
+	},
+}));
+
+interface TransactionRowProps {
+	header: any;
+	content: any;
+}
+
+const TransactionRow: React.FC<TransactionRowProps> = ({ header, content }) => {
+	const classes = useMainStyles();
+	return (
+		<Grid container spacing={2} className={classes.transactionRow} alignItems="center">
+			<Grid item xs={4}>
+				{header}
+			</Grid>
+			<Grid item xs={8}>
+				<Typography variant="body2" color="textPrimary">
+					{content}
+				</Typography>
+			</Grid>
+		</Grid>
+	);
+};
+
+const transactionRows = [
+	['Token', 'eClaw FEB29'],
+	['Amount', 234.23],
+	['Completes On', 'Feb 29th, 2021 @ 12:00 MST'],
+	['Locked Collateral', 0.013],
+	['Liquidated Collateral', 0.012],
+	['Settlement Price', '$63,393'],
+	['Fee', '$33.33'],
+	['Liquidator', '0x0BC9ce5dE1Ed7a4e31EB1A33Aef43D4d1057F8C6'],
+	['Disputer', '0x0BC9ce5dE1Ed7a4e31EB1A33Aef43D4d1057F8C6'],
+];
+
+interface Props {
+	isOpen?: boolean;
+}
+
+export const LiquidationDialog: React.FC<Props> = ({ isOpen = false }) => {
+	const classes = useMainStyles();
+
+	return (
+		<Dialog maxWidth="sm" fullWidth={true} aria-labelledby="liquidation-dialog" open={isOpen}>
+			<DialogTitle className={classes.dialogTitle} id="liquidation-dialog-title">
+				eCLAW FEB29 Transaction
+			</DialogTitle>
+			<Divider variant="middle" className={classes.mx} />
+			<DialogContent className={classes.dialogContent}>
+				{/* {transactionRows.forEach((row) => {
+					return <TransactionRow header={row[0]} content={row[1]}/>
+				})} */}
+				<TransactionRow header={transactionRows[0][0]} content={transactionRows[0][1]} />
+				<TransactionRow header={transactionRows[1][0]} content={transactionRows[1][1]} />
+				<TransactionRow header={transactionRows[2][0]} content={transactionRows[2][1]} />
+				<TransactionRow header={transactionRows[3][0]} content={transactionRows[3][1]} />
+				<TransactionRow header={transactionRows[4][0]} content={transactionRows[4][1]} />
+				<TransactionRow header={transactionRows[5][0]} content={transactionRows[5][1]} />
+				<TransactionRow header={transactionRows[6][0]} content={transactionRows[6][1]} />
+				<TransactionRow header={transactionRows[7][0]} content={transactionRows[7][1]} />
+				<TransactionRow header={transactionRows[8][0]} content={transactionRows[8][1]} />
+			</DialogContent>
+			<Divider variant="middle" className={classes.mx} />
+			<DialogActions className={classes.dialogActions}>
+				<Button color="primary">Cancel</Button>
+			</DialogActions>
+		</Dialog>
+	);
+};
