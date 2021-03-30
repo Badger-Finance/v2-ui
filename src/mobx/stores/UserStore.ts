484ab76e532c45c0299afc73f423655defb8461e
jintao <hellojintao@protonmail.com>
2021-03-30 17:19:11 -0400

    feat: add sett store link for eligibile users

---
+++ src/mobx/stores/UserStore.ts
@@ -0,0 +1,63 @@
+import { extendObservable, action, observe } from 'mobx';
+import { RootStore } from '../store';
+import { UserPermissions } from 'mobx/model';
+import { checkShopEligibility } from 'mobx/utils/apiV2';
+import WalletStore from './walletStore';
+
+/**
+ * TODO: Aggregation of user specific information will move here.
+ * This includes:
+ *   - rewards claim proofs
+ *   - airdrop claim proofs
+ *   - yearn bouncer proofs
+ *   - sett shop eligibility
+ * We can eventually merge this with disjoint pieces of the wallet
+ * store once pieces have been incorporated into the @see {User}
+ * model to support wallet features, etc.
+ */
+export default class UserStore {
+	private store!: RootStore;
+
+	// loading: undefined, error: null, present: object
+	private permissions: UserPermissions | undefined | null;
+
+	constructor(store: RootStore) {
+		this.store = store;
+
+		extendObservable(this, {
+			permissions: undefined,
+		});
+
+		/**
+		 * TODO: Update with a full user address data update on change.
+		 */
+		observe(this.store.wallet as WalletStore, 'connectedAddress', () => {
+			const address = this.store.wallet.connectedAddress;
+			if (address) {
+				this.getSettShopEligibility(address);
+			}
+		});
+
+		this.permissions = undefined;
+	}
+
+	get viewSettShop(): boolean {
+		if (!this.permissions) {
+			return false;
+		}
+		return this.permissions.viewSettShop;
+	}
+
+	getSettShopEligibility = action(
+		async (address: string): Promise<void> => {
+			if (address) {
+				const eligibility = await checkShopEligibility(address);
+				if (eligibility) {
+					this.permissions = {
+						viewSettShop: eligibility.isEligible,
+					};
+				}
+			}
+		},
+	);
+}
