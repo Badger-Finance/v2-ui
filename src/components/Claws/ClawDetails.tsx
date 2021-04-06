74809dd64d6979aeae13ca9a69114f42ceca098b
Elio <eliobricenov@gmail.com>
2021-04-06 01:13:43 -0400

    feat: start align detail name on mobile

--- src/components/Claws/ClawDetails.tsx
+++ src/components/Claws/ClawDetails.tsx
@@ -1,6 +1,6 @@
 /* eslint-disable react/prop-types */
 import React, { FC } from 'react';
-import { Grid, Typography, makeStyles } from '@material-ui/core';
+import { Grid, Typography, makeStyles, useMediaQuery } from '@material-ui/core';
 
 interface Detail {
 	name: string;
@@ -18,7 +18,14 @@ const useStyles = makeStyles((theme) => ({
 	row: {
 		marginTop: theme.spacing(1),
 	},
-	detail: {
+	detailName: {
+		wordBreak: 'break-word',
+		textAlign: 'end',
+		'@media (max-width: 480px)': {
+			textAlign: 'start',
+		},
+	},
+	detailDescription: {
 		wordBreak: 'break-word',
 		textAlign: 'end',
 	},
@@ -26,19 +33,13 @@ const useStyles = makeStyles((theme) => ({
 		wordBreak: 'break-word',
 		textAlign: 'center',
 	},
-	rightMargin: {
-		marginRight: theme.spacing(2),
-	},
-	leftMargin: {
-		marginLeft: theme.spacing(2),
-	},
 }));
 
-const DetailName = ({ name }: Pick<Detail, 'name'>) => {
+const DetailName = ({ name, text }: Pick<Detail, 'name' | 'text'>) => {
 	const classes = useStyles();
 
 	return (
-		<Grid item xs={6} className={[classes.detail].join(' ')}>
+		<Grid item xs={6} className={classes.detailName}>
 			<Typography variant="body2" color="textSecondary">
 				{name}
 			</Typography>
@@ -48,9 +49,10 @@ const DetailName = ({ name }: Pick<Detail, 'name'>) => {
 
 const DetailDescription = ({ text, subText }: Pick<Detail, 'text' | 'subText'>) => {
 	const classes = useStyles();
+	const descriptionClass = text ? classes.detailDescription : classes.placeholder;
 
 	return (
-		<Grid item container xs={6} className={[text ? classes.detail : classes.placeholder].join(' ')}>
+		<Grid item container xs={6} className={descriptionClass}>
 			<Grid item xs={12}>
 				<Typography variant="body2" color="textPrimary">
 					{text || '-'}
@@ -74,7 +76,7 @@ export const ClawDetails: FC<Props> = ({ details }) => {
 		<Grid container className={classes.root}>
 			{details.map(({ name, text, subText }, index) => (
 				<Grid container className={classes.row} key={`${name}_${index}`}>
-					<DetailName name={name} />
+					<DetailName name={name} text={text} />
 					<DetailDescription text={text} subText={subText} />
 				</Grid>
 			))}
