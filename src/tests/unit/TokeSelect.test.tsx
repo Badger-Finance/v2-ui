0619c08f9f8e35404c63985859c5cd1406e957b2
Elio <eliobricenov@gmail.com>
2021-04-12 15:18:08 -0400

    test: add unit test for token select

---
+++ src/tests/unit/TokeSelect.test.tsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import '@testing-library/jest-dom';
+import { screen, customRender, fireEvent, within } from '../Utils';
+import { TokenSelect } from '../../components-v2/common/TokenSelect';
+
+test('displays placeholder', () => {
+	customRender(<TokenSelect placeholder={'Select Token'} onChange={jest.fn()} />);
+	expect(screen.getByRole('button', { name: 'Select Token' })).toBeInTheDocument();
+});
+
+test('displays options', () => {
+	const handleChange = jest.fn();
+	customRender(
+		<TokenSelect
+			placeholder={'Select Token'}
+			options={
+				new Map<string, string>(
+					Object.entries({
+						'0x123': 'TestToken',
+					}),
+				)
+			}
+			onChange={handleChange}
+		/>,
+	);
+
+	const selector = screen.getByRole('button', { name: 'Select Token' });
+	fireEvent.mouseDown(selector);
+	const list = within(screen.getByRole('listbox'));
+	expect(list.getByText('TestToken')).toBeInTheDocument();
+});
+
+test('returns correct value on change', () => {
+	const handleChange = jest.fn();
+	customRender(
+		<TokenSelect
+			placeholder={'Select Token'}
+			options={
+				new Map<string, string>(
+					Object.entries({
+						'0x123': 'TestToken',
+					}),
+				)
+			}
+			onChange={handleChange}
+		/>,
+	);
+
+	const selector = screen.getByRole('button', { name: 'Select Token' });
+	fireEvent.mouseDown(selector);
+	const list = within(screen.getByRole('listbox'));
+	fireEvent.click(list.getByText('TestToken'));
+	expect(handleChange).toHaveBeenNthCalledWith(1, '0x123');
+});
