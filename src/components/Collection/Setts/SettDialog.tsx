54d7f8b617d6e55f0393c54b6ff7f60b768022fd
Ben Walker <ben.walker18@gmail.com>
2021-04-22 16:10:55 -0700

    Fix ESlint warning

--- src/components/Collection/Setts/SettDialog.tsx
+++ src/components/Collection/Setts/SettDialog.tsx
@@ -34,6 +34,7 @@ const SettDialog = (props: SettDialogProps): JSX.Element => {
 	const { open, sett } = dialogProps;
 	let { vault } = dialogProps;
 	const store = useContext(StoreContext);
+	// eslint-disable-next-line @typescript-eslint/no-unused-vars
 	const { network, connectedAddress } = store.wallet;
 	const { contracts } = store;
 	const classes = useStyles();
