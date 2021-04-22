4e585ed215c5cf957d7968d7df3698897611e68a
carlos <cfebres@fullstacklabs.co>
2021-04-21 23:01:29 -0400

    test: ganache implementation

--- src/tests/ibBTC/Mint.test.tsx
+++ src/tests/ibBTC/Mint.test.tsx
@@ -1,67 +1,39 @@
-import React from 'react';
-import BigNumber from 'bignumber.js';
-import '@testing-library/jest-dom';
-import { action } from 'mobx';
+import Ganache from 'ganache-core';
+import Web3 from 'web3';
+import store from 'mobx/store';
 
-import { customRender, screen } from '../Utils';
-import addresses from 'config/ibBTC/addresses.json';
-import store from '../../mobx/store';
-import { StoreProvider } from '../../mobx/store-context';
-import { TokenModel } from '../../mobx/model';
-import { Mint } from '../../components/IbBTC/Mint';
+const PRIV_KEY_ONE = '0x990b68b61853f6418233b1f502a220a8770bb38849d9bd8fc552ed55f5899365';
 
-// this is a workaround for the 'TypeError: document.createRange is not a function' error using the tooltip
-// see https://github.com/mui-org/material-ui/issues/15726
-(global as any).document.createRange = () => ({
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	setStart: () => {},
-	// eslint-disable-next-line @typescript-eslint/no-empty-function
-	setEnd: () => {},
-	commonAncestorContainer: {
-		nodeName: 'BODY',
-		ownerDocument: document,
-	},
-});
+export const startChain = () => {
+	const provider = Ganache.provider({
+		fork: 'https://mainnet.infura.io/v3/4c06e2847e1d456ea30506468ad0be5c',
+		network_id: 1,
+		accounts: [
+			{
+				secretKey: PRIV_KEY_ONE,
+				balance: Web3.utils.toHex(1000),
+			},
+		],
+	});
+	const web3 = new Web3(provider as any);
+	const wallet = web3.eth.accounts.privateKeyToAccount(PRIV_KEY_ONE);
+	return { provider, wallet, web3 };
+};
 
-const mockCalcMintAmount = action(
-	jest.fn().mockImplementation(
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		async (inToken: TokenModel, amount: BigNumber, callback: (err: any, result: any) => void) => {
-			callback(null, [new BigNumber('20').multipliedBy(10 ** 18), new BigNumber('0.010').multipliedBy(10 ** 18)]);
-		},
-	),
-);
+jest.setTimeout(30000);
 
-const mockIbtc = new TokenModel(store, addresses.mainnet.contracts.tokens.ibBTC);
-const mockTokens = [
-	new TokenModel(store, addresses.mainnet.contracts.tokens['bcrvRenWSBTC']),
-	new TokenModel(store, addresses.mainnet.contracts.tokens['bcrvRenWBTC']),
-	new TokenModel(store, addresses.mainnet.contracts.tokens['btbtc/sbtcCrv']),
-];
-
-store.ibBTCStore.init = action(jest.fn());
-store.ibBTCStore.calcMintAmount = mockCalcMintAmount;
-store.ibBTCStore.ibBTC = mockIbtc;
-store.ibBTCStore.tokens = mockTokens;
-
-it('displays tokens list with correct default token', () => {
-	const defaultToken = mockTokens[0];
-	customRender(
-		<StoreProvider value={store}>
-			<Mint />
-		</StoreProvider>,
-	);
-	// the select uses the token image and the token name
-	expect(screen.getByRole('button', { name: `${defaultToken.name} ${defaultToken.symbol}` })).toBeInTheDocument();
-});
-
-it('displays tokens list', () => {
-	const defaultToken = mockTokens[0];
-	customRender(
-		<StoreProvider value={store}>
-			<Mint />
-		</StoreProvider>,
-	);
-
-	expect(screen.getByRole('button', { name: `${defaultToken.name} ${defaultToken.symbol}` })).toBeInTheDocument();
+it('should work (hopefully)', () => {
+	const { wallet, web3 } = startChain();
+	// store.wallet.provider = provider;
+	// store.wallet.connectedAddress = wallet.address;
+	return new Promise((resolve) => {
+		web3.eth.getBalance(wallet.address, function (err, result) {
+			if (err) {
+				console.log(err);
+			} else {
+				console.log(web3.utils.fromWei(result, 'ether') + ' ETH');
+			}
+			resolve(result);
+		});
+	});
 });
