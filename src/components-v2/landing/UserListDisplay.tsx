a458d3ffa4a69c45eb9279ceb4266fd6ad7acead
jintao <hellojintao@protonmail.com>
2021-05-10 10:55:46 -0400

    feat: display leaderboard w/ pagination

--- src/components-v2/landing/UserListDisplay.tsx
+++ src/components-v2/landing/UserListDisplay.tsx
@@ -1,4 +1,5 @@
 import { makeStyles, Typography } from '@material-ui/core';
+import clsx from 'clsx';
 import { Loader } from 'components/Loader';
 import { observer } from 'mobx-react-lite';
 import {
@@ -17,15 +18,55 @@ import { SettListViewProps } from './SettListView';
 import SettTable from './SettTable';
 
 const useStyles = makeStyles((theme) => ({
+	icon: {
+		marginLeft: theme.spacing(4),
+		marginRight: theme.spacing(4),
+		marginBottom: theme.spacing(4),
+		[theme.breakpoints.down('sm')]: {
+			marginLeft: theme.spacing(2),
+			marginRight: theme.spacing(2),
+			marginBottom: theme.spacing(2),
+		},
+	},
 	boostIcon: {
-		marginLeft: theme.spacing(1),
-		marginRight: theme.spacing(1),
+		height: 55,
+		width: 55,
+		[theme.breakpoints.down('sm')]: {
+			height: 38,
+			width: 38,
+		},
+	},
+	headIcon: {
+		height: 48,
+		width: 48,
+		[theme.breakpoints.down('sm')]: {
+			height: 30,
+			width: 30,
+		},
 	},
 	boostContainer: {
 		display: 'flex',
 		justifyContent: 'center',
 		alignItems: 'center',
-		marginBottom: theme.spacing(4),
+		marginBottom: theme.spacing(3),
+		[theme.breakpoints.down('sm')]: {
+			marginBottom: theme.spacing(2),
+		},
+	},
+	boostInfo: {
+		flexDirection: 'column',
+	},
+	boostText: {
+		fontSize: '2.5rem',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '1.4rem',
+		},
+	},
+	boostRankText: {
+		fontSize: '1.3rem',
+		[theme.breakpoints.down('sm')]: {
+			fontSize: '0.8rem',
+		},
 	},
 }));
 
@@ -129,9 +170,12 @@ const UserListDisplay = observer((props: SettListViewProps) => {
 		<>
 			{accountDetails && (
 				<div className={classes.boostContainer}>
-					<img className={classes.boostIcon} src="./assets/icons/badger_head.svg" height={25} width={25} />
-					<Typography variant="h5">Badger Boost: {accountDetails.boost.toFixed(2)}</Typography>
-					<img className={classes.boostIcon} src="./assets/icons/badger_saiyan.png" height={28} width={28} />
+					<img className={clsx(classes.icon, classes.headIcon)} src="./assets/icons/badger_head.svg" />
+					<div className={clsx(classes.boostInfo, classes.boostContainer)}>
+						<Typography className={classes.boostText}>Boost: {accountDetails.boost.toFixed(2)}</Typography>
+						<Typography className={classes.boostRankText}>Rank: {accountDetails.boostRank}</Typography>
+					</div>
+					<img className={clsx(classes.icon, classes.boostIcon)} src="./assets/icons/badger_saiyan.png" />
 				</div>
 			)}
 			{displayWallet && (
---
+++ src/components-v2/leaderboard/styles.tsx
@@ -0,0 +1,5 @@
+import { styled, TableCell } from '@material-ui/core';
+
+export const LeaderBoardCell = styled(TableCell)(() => ({
+	borderBottom: 'none',
+}));
