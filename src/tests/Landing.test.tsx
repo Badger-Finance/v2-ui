9df438c38b44d8300b925f0f08c5acfbb9c16c82
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2021-05-17 10:37:01 -0400

    test: mocked functions for landing tests

--- src/tests/Landing.test.tsx
+++ src/tests/Landing.test.tsx
@@ -4,13 +4,37 @@ import Landing from '../pages/Landing';
 import '@testing-library/jest-dom';
 import { StoreProvider } from '../mobx/store-context';
 import store from '../mobx/store';
+import { TokenModel } from 'mobx/model';
+import addresses from 'config/ibBTC/addresses.json';
+import BigNumber from 'bignumber.js';
 
-afterEach(cleanup);
+const tokensConfig = addresses.mainnet.contracts.tokens;
+const mockIbBTC = new TokenModel(store, tokensConfig['ibBTC']);
 
 describe('Landing Page', () => {
 	const connectedStore = store;
+	connectedStore.ibBTCStore.ibBTC = mockIbBTC;
 
 	connectedStore.wallet.connectedAddress = '0x1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a';
+
+	beforeEach(() => {
+		connectedStore.ibBTCStore.getMintLimit = jest.fn().mockReturnValue({
+			userLimit: new BigNumber('5000000000'),
+			allUsersLimit: new BigNumber('10000000000'),
+			individualLimit: new BigNumber('5000000000'),
+			globalLimit: new BigNumber('10000000000'),
+		});
+		jest.spyOn(connectedStore.ibBTCStore, 'fetchBalance').mockImplementation(async () => {
+			return new BigNumber('100');
+		});
+		jest.spyOn(connectedStore.honeyPot, 'fetchNFTS').mockImplementation(async () => {
+			new Promise(() => {
+				return;
+			});
+		});
+	});
+
+	afterEach(cleanup);
 
 	test('Renders correctly', () => {
 		const { container } = customRender(
