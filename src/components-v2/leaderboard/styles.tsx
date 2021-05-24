dd01138def212f5b536c2e8fbb0773188e8d9417
jintao <hellojintao@protonmail.com>
2021-05-24 10:01:49 -0400

    chore: add leaderboard stake ratio

--- src/components-v2/leaderboard/styles.tsx
+++ src/components-v2/leaderboard/styles.tsx
@@ -1,5 +1,8 @@
 import { styled, TableCell } from '@material-ui/core';
 
-export const LeaderBoardCell = styled(TableCell)(() => ({
+export const LeaderBoardCell = styled(TableCell)(({ theme }) => ({
 	borderBottom: 'none',
+	[theme.breakpoints.down('sm')]: {
+		paddingBottom: 0,
+	},
 }));
