3dbc02cae1ba75c009a27a4f7e8b756c8c0ba5b3
Elio <eliobricenov@gmail.com>
2021-06-16 12:45:55 -0400

    refactor: show boost calculator when flagged on

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -92,6 +92,7 @@ export const FLAGS = {
 	IBBTC_OPTIONS_FLAG: toBool(process.env.REACT_APP_IBBTC_OPTIONS_FLAG),
 	CONVEX_SETTS: toBool(process.env.REACT_APP_CONVEX_SETTS),
 	STABILIZATION_SETTS: toBool(process.env.REACT_APP_STABILIZATION_SETTS),
+	BOOST_CALCULATOR: toBool(process.env.REACT_APP_BOOST_CALCULATOR),
 };
 
 export const ZERO = new BigNumber(0);
--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -32,7 +32,7 @@ const routes = {
 	}),
 	boost: new Route<RootStore, QueryParams>({
 		path: '/boost',
-		component: <Boost />,
+		component: FLAGS.BOOST_CALCULATOR ? <Boost /> : <></>,
 	}),
 	digg: new Route<RootStore, QueryParams>({
 		path: '/digg',
