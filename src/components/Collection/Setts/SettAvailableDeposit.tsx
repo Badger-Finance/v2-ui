6d8b93b6079d471073b0d1f613c018eca77e0075
jintao <hellojintao@protonmail.com>
2021-06-17 15:37:11 -0400

    chore: update modal, tests

--- src/components/Collection/Setts/SettAvailableDeposit.tsx
+++ src/components/Collection/Setts/SettAvailableDeposit.tsx
@@ -8,12 +8,19 @@ const useStyles = makeStyles((theme) => ({
 		justifyContent: 'space-around',
 		alignItems: 'center',
 		marginBottom: theme.spacing(2),
+		[theme.breakpoints.down('xs')]: {
+			flexDirection: 'column',
+			marginBottom: theme.spacing(1),
+		},
 	},
 	depositContainer: {
 		display: 'flex',
 		flexDirection: 'column',
 		justifyContent: 'center',
 		alignItems: 'center',
+		[theme.breakpoints.down('xs')]: {
+			marginBottom: theme.spacing(1),
+		},
 	},
 }));
 
@@ -21,28 +28,31 @@ export interface DepositLimitProps {
 	vaultCapInfo: VaultCap;
 }
 
-export const SettAvailableDeposit = (props: DepositLimitProps): JSX.Element => {
-	const { vaultCapInfo } = props;
-	const { vaultCap, totalVaultCap, userCap, totalUserCap, asset } = vaultCapInfo;
+export const SettAvailableDeposit = (props: DepositLimitProps): JSX.Element | null => {
 	const displayDecimals = 4;
 	const classes = useStyles();
+	const { vaultCapInfo } = props;
+	if (!vaultCapInfo) {
+		return null;
+	}
+	const { vaultCap, totalVaultCap, userCap, totalUserCap, asset } = vaultCapInfo;
 	return (
 		<div className={classes.limitsContainer}>
 			<div className={classes.depositContainer}>
-				<Typography variant="body2" color="textSecondary">
+				<Typography align="center" variant="body2" color="textSecondary">
 					User Deposit Limit Remaining:{' '}
 				</Typography>
-				<Typography variant="body2" color="textSecondary" component="div">
+				<Typography align="center" variant="body2" color="textSecondary" component="div">
 					{`${userCap.balanceDisplay(displayDecimals)} / ${totalUserCap.balanceDisplay(
 						displayDecimals,
 					)} ${asset}`}
 				</Typography>
 			</div>
 			<div className={classes.depositContainer}>
-				<Typography variant="body2" color="textSecondary">
+				<Typography align="center" variant="body2" color="textSecondary">
 					Total Deposit Limit Remaining:{' '}
 				</Typography>
-				<Typography variant="body2" color="textSecondary" component="div">
+				<Typography align="center" variant="body2" color="textSecondary" component="div">
 					{`${vaultCap.balanceDisplay(displayDecimals)} / ${totalVaultCap.balanceDisplay(
 						displayDecimals,
 					)} ${asset}`}
