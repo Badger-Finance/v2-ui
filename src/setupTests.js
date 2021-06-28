0df193d54178bf7076a11d5661715b79696bf026
Phil Eaton <phil@eatonphil.com>
2021-06-28 15:25:03 +0000

    Everything but the tests working...

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,6 +1,7 @@
 // See https://github.com/facebook/jest/issues/9983
 import { TextEncoder, TextDecoder } from 'util';
-export default function (): void {
-	global.TextEncoder = TextEncoder;
-	global.TextDecoder = TextDecoder;
+global.TextEncoder = TextEncoder;
+global.TextDecoder = TextDecoder;
+export default function () {
+	return null;
 }
