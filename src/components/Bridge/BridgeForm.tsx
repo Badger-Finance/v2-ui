e4b189dee4ab20cd7c148f62392ecbcc766d5a1e
DefNotAB0t <78719137+DefNotAB0t@users.noreply.github.com>
2021-06-29 22:30:30 -0400

    adding calcmint/calcredeem pathing functions
    adding calcmint/calcredeem pathing functions
    
    adding calcmint/calcredeem pathing functions

--- src/components/Bridge/BridgeForm.tsx
+++ src/components/Bridge/BridgeForm.tsx
@@ -287,7 +287,7 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 			begin,
 			loading,
 			error,
-			calcMintAndRedeemPath,
+			calcMintOrRedeemPath,
 
 			badgerBurnFee,
 			badgerMintFee,
@@ -403,7 +403,7 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 		}));
 	};
 	//not sure if there's a better way to do this than this ugly nested switch statement
-	const vaultAddress = () => {
+	const vaultAddress = (poolId?: number) => {
 		switch (token) {
 			case 'byvWBTC':
 				return sett_system.vaults['yearn.wBtc'];
@@ -445,6 +445,8 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 				return sett_system.vaults['native.sbtcCrv'];
 			case 'bCRVtBTC':
 				return sett_system.vaults['native.tbtcCrv'];
+			case 'ibBTC':
+				return tokens.ibBTC;
 			default:
 				return '0x0000000000000000000000000000000000000000';
 		}
@@ -497,14 +499,17 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 			maxSlippageBps = Math.round(parseFloat(maxSlippage) * 100);
 			desiredToken = tokens.wBTC;
 		}
+
 		let ibBTCFlag = false;
+		let poolId = undefined;
 		if (token === 'ibBTC') {
 			ibBTCFlag = true;
-			await calcMintAndRedeemPath(desiredToken.toString(), amountSats);
-			console.log('token is ibbtc');
+			const [id, amount, optToken] = await calcMintOrRedeemPath(amountSats, true);
+			if (optToken !== undefined && id !== undefined) {
+				desiredToken = optToken;
+				poolId = parseInt(id);
+			}
 		}
-
-		console.log(poolId);
 
 		const contractParams: EthArgs = [
 			{
@@ -526,7 +531,7 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 				name: '_vault',
 				type: 'address',
 				// Will check in SC if address is addres(0), if not, will deposit to the desired vault
-				value: vaultAddress(),
+				value: vaultAddress(poolId),
 			},
 			{
 				name: '_mintIbbtc',
@@ -566,6 +571,17 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 			maxSlippageBps = Math.round(parseFloat(maxSlippage) * 100);
 		}
 
+		let ibBTCFlag = false;
+		let poolId = undefined;
+		if (token === 'ibBTC') {
+			ibBTCFlag = true;
+			const [id, amount, optToken] = await calcMintOrRedeemPath(amountOut, false);
+			if (optToken !== undefined && id !== undefined) {
+				burnToken = optToken;
+				poolId = parseInt(id);
+			}
+		}
+
 		const params: EthArgs = [
 			{
 				name: '_token',
@@ -592,6 +608,11 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 				name: '_amount',
 				type: 'uint256',
 				value: amountOut.toString(),
+			},
+			{
+				name: '_burnIbbtc',
+				type: 'bool',
+				value: ibBTCFlag,
 			},
 		];
 
@@ -665,6 +686,19 @@ export const BridgeForm = observer(({ classes, tabValue, setTabValue }: any) => 
 		if (token === 'WBTC' || token === 'byvWBTC') {
 			estimatedSlippage = await getEstimatedSlippage(amountWithFee, name);
 			amountWithFee *= 1 - estimatedSlippage;
+		}
+
+		//backspacing in the textbox caused issues without the second part of if statement
+		if (token === 'ibBTC' && inputAmount.toString() !== '') {
+			const bigInputAmount = new BigNumber(inputAmount);
+			let mintBool = true;
+			if (name === 'burnAmount') {
+				mintBool = false;
+			}
+			const [id, amount, optToken] = await calcMintOrRedeemPath(bigInputAmount, mintBool);
+			if (amount !== undefined) {
+				amountWithFee = Number(amount);
+			}
 		}
 
 		setStates((prevState) => ({
