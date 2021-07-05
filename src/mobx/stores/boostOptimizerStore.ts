618042e55ce9d695dcac7c49ee42f2f72d29914a
Elio <eliobricenov@gmail.com>
2021-07-05 14:58:06 -0400

    refactor: use interpolation for amount $ to native calculation

--- src/mobx/stores/boostOptimizerStore.ts
+++ src/mobx/stores/boostOptimizerStore.ts
@@ -2,6 +2,12 @@ import BigNumber from 'bignumber.js';
 
 import deploy from '../../config/deployments/mainnet.json';
 import { RootStore } from '../store';
+import { percentageBetweenRange } from '../../utils/componentHelpers';
+import { LeaderBoardEntry } from '../model';
+import { ZERO } from '../../config/constants';
+
+// linear interpolation
+const lerp = (a: number, b: number, t: number) => a + (b - a) * t;
 
 export class BoostOptimizerStore {
 	constructor(private store: RootStore) {
@@ -49,8 +55,8 @@ export class BoostOptimizerStore {
 	}
 
 	/**
-	 * Calculates what rank position would a specific boost have in the leaderboard
-	 * @param boost target boost
+	 * Calculates what rank position would a specific boost have in the leaderboard.
+	 * Returns first entry whose boost value is less or equal to the given boost.
 	 */
 	calculateRankFromBoost(boost: number): number | undefined {
 		const { completeBoard: leaderBoard } = this.store.leaderBoard;
@@ -122,6 +128,9 @@ export class BoostOptimizerStore {
 	/**
 	 * Calculate how much USD would be required in native assets to match a desired boost given
 	 * a set of native and non-native balances.
+	 *
+	 * This is done by calculating the differences between the boost ratio generated from the given
+	 * native and non native values and the boost ratio calculated using the desired boost.
 	 * @param native native's balance
 	 * @param nonNative non-native's balance
 	 * @param desiredBoost target boost
@@ -132,27 +141,31 @@ export class BoostOptimizerStore {
 		desiredBoost: number,
 	): BigNumber | undefined {
 		const { completeBoard: leaderBoard } = this.store.leaderBoard;
-
-		if (!leaderBoard) {
-			return;
-		}
-
 		const boostRatio = this.calculateBoostRatio(native, nonNative);
-
-		if (boostRatio === -1) {
-			return;
-		}
-
 		const rankUsingDesiredBoost = this.findPositionInLeaderboardFromBoost(desiredBoost);
 
-		if (rankUsingDesiredBoost === undefined) {
+		if (!leaderBoard || boostRatio === -1 || rankUsingDesiredBoost === undefined) {
 			return;
 		}
 
-		const positionToOvertakeRatio = leaderBoard[rankUsingDesiredBoost].stakeRatio;
-		const ratioToTargetBoost = BigNumber.max(new BigNumber(positionToOvertakeRatio).minus(boostRatio), 0);
+		const rankPositionToOvertake = leaderBoard[rankUsingDesiredBoost];
+		const superiorRank = leaderBoard[rankUsingDesiredBoost - 1];
+		const inferiorEntry = leaderBoard[rankUsingDesiredBoost];
 
-		return ratioToTargetBoost.multipliedBy(nonNative).multipliedBy(1.1);
+		// if current boost ratio is greater or equal to the desired boost ratio there's no need for extra money
+		if (boostRatio >= Number(rankPositionToOvertake.stakeRatio)) {
+			return ZERO;
+		}
+
+		// if we don't have a superior entry that means we've reached the top of the rank and we can calculate
+		// the amount missing with the difference in the ratios
+		if (!superiorRank) {
+			const rankPositionToOvertakeRatio = Number(rankPositionToOvertake.stakeRatio);
+			return this.differenceInRatios(boostRatio, rankPositionToOvertakeRatio).multipliedBy(nonNative);
+		}
+
+		// interpolate the ratio using the superior and inferior leaderboard entries
+		return this.interpolateDifferenceInRatios(desiredBoost, superiorRank, inferiorEntry).multipliedBy(nonNative);
 	}
 
 	/**
@@ -203,5 +216,36 @@ export class BoostOptimizerStore {
 		}
 
 		return leaderBoard.length - 1;
+	};
+
+	/**
+	 * Calculate the difference between two given boost ratios. If difference is negative then zero is returned
+	 */
+	private differenceInRatios = (ratioA: number, ratioB: number): BigNumber => {
+		return BigNumber.max(new BigNumber(ratioB).minus(ratioA), 0);
+	};
+
+	/**
+	 * Calculate the difference in ratios indirectly by using a desired boost.
+	 *
+	 * This is done using a linear interpolation using the given superior and inferior entries as range data points
+	 * and the desired boost as the middle point
+	 * @param desiredBoost
+	 * @param slotSuperior
+	 * @param slotInferior
+	 */
+	private interpolateDifferenceInRatios = (
+		desiredBoost: number,
+		slotSuperior: LeaderBoardEntry,
+		slotInferior: LeaderBoardEntry,
+	): BigNumber => {
+		const userBoostSuperior = Number(slotSuperior.boost);
+		const userBoostInferior = Number(slotInferior.boost);
+		const stakeRatioFromBoost = percentageBetweenRange(desiredBoost, userBoostSuperior, userBoostInferior);
+
+		const userStakeRatioSuperior = Number(slotSuperior.stakeRatio);
+		const userStakeRatioInferior = Number(slotInferior.stakeRatio);
+
+		return new BigNumber(lerp(userStakeRatioInferior, userStakeRatioSuperior, stakeRatioFromBoost));
 	};
 }
