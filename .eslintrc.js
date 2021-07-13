15099b1076c487d769456854f1a95c57decae2e5
Sajan Rajdev <25463788+sajanrajdev@users.noreply.github.com>
2021-07-13 13:23:59 -0400

    lint: added promise handling linting rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,15 @@
 module.exports = {
 	parser: '@typescript-eslint/parser', // Specifies the ESLint parser
-	plugins: ['autofix', 'react', 'react-hooks', '@typescript-eslint', 'prettier', 'unused-imports'],
+	plugins: [
+		'autofix',
+		'react',
+		'react-hooks',
+		'@typescript-eslint',
+		'prettier',
+		'unused-imports',
+		'promise',
+		'no-floating-promise',
+	],
 	parserOptions: {
 		ecmaVersion: 2020, // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module', // Allows for the use of imports
@@ -18,6 +27,7 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended', // Uses the recommended rules from @typescript-eslint/eslint-plugin
 		'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
 		'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+		'plugin:promise/recommended',
 	],
 	rules: {
 		// Place to specify ESLint rules. Can be used to overwrite rules specified from the extended configs
@@ -35,5 +45,6 @@ module.exports = {
 				endOfLine: 'auto',
 			},
 		],
+		'no-floating-promise/no-floating-promise': 2,
 	},
 };
