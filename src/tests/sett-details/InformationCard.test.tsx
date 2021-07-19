0b18fb228ad0eeb65a912749e7c618352058e569
Elio <eliobricenov@gmail.com>
2021-07-19 17:38:36 -0400

    refactor: rename section name

--- src/tests/sett-details/InformationCard.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { checkSnapshot } from 'tests/utils/snapshots';
-import { InformationCard } from '../../components-v2/sett-detail/information-card/InformationCard';
-
-describe('Information Card', () => {
-	it('displays sett information', () => {
-		checkSnapshot(<InformationCard />);
-	});
-});
