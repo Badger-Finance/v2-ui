eaa698398831d93ffef0c50d2d13359b9fd7795f
Elio <eliobricenov@gmail.com>
2021-07-20 18:13:00 -0400

    wip: add markup and styles for header description

--- src/components-v2/sett-detail/Content.tsx
+++ src/components-v2/sett-detail/Content.tsx
@@ -1,22 +1,39 @@
 import React from 'react';
-import { Grid, makeStyles } from '@material-ui/core';
+import { Grid, makeStyles, useMediaQuery, useTheme } from '@material-ui/core';
 import { SpecsCard } from './specs/SpecsCard';
+import { Description } from './Description';
+import { ActionButtons } from './ActionButtons';
+import { Breadcrumb } from './Breadcrumb';
 
 const useStyles = makeStyles((theme) => ({
 	content: {
-		marginTop: theme.spacing(3),
 		margin: 'auto',
 		[theme.breakpoints.up('md')]: {
-			marginTop: theme.spacing(5),
+			marginTop: theme.spacing(2),
 		},
+	},
+	descriptionSection: {
+		justifyContent: 'space-between',
+		marginBottom: theme.spacing(5),
+	},
+	breadcrumbContainer: {
+		marginBottom: theme.spacing(1),
 	},
 }));
 
 export const Content = (): JSX.Element => {
 	const classes = useStyles();
+	const canFitActionButtons = useMediaQuery(useTheme().breakpoints.up(680));
 
 	return (
 		<Grid container className={classes.content}>
+			<Grid container className={classes.breadcrumbContainer}>
+				<Breadcrumb />
+			</Grid>
+			<Grid container className={classes.descriptionSection}>
+				<Description />
+				{canFitActionButtons && <ActionButtons />}
+			</Grid>
 			<Grid item xs={12} lg={4}>
 				<SpecsCard />
 			</Grid>
