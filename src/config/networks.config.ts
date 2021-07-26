9f9ff0d1e2d771824eba0d2974916272a152a916
jintao <hellojintao@protonmail.com>
2021-07-26 16:06:16 -0400

    chore: add xdai support

--- src/config/networks.config.ts
+++ src/config/networks.config.ts
@@ -2,6 +2,7 @@ import { BinanceSmartChain } from 'mobx/model/network/bsc.network';
 import { Ethereum } from 'mobx/model/network/eth.network';
 import { Polygon } from 'mobx/model/network/matic.network';
 import { Network } from 'mobx/model/network/network';
+import { xDai } from 'mobx/model/network/xdai.network';
 
-export const supportedNetworks: Network[] = [new Ethereum(), new Polygon(), new BinanceSmartChain()];
+export const supportedNetworks: Network[] = [new Ethereum(), new Polygon(), new BinanceSmartChain(), new xDai()];
 export const defaultNetwork = supportedNetworks[0];
