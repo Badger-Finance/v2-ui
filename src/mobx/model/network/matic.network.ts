208e4298bebe61e8047d261d0f867207d7b48b78
mitche50 <mitche50@gmail.com>
2021-08-03 20:26:31 -0400

    chore: add matic ibbtc wbtc vault

--- src/mobx/model/network/matic.network.ts
+++ src/mobx/model/network/matic.network.ts
@@ -21,6 +21,9 @@ export class Polygon extends NetworkModel {
 			maticVaults,
 		);
 	}
+	get settOrder(): string[] {
+		return [this.deploy.sett_system.vaults['BSLP-IBBTC-WBTC'], this.deploy.sett_system.vaults['BQLP-BADGER-USDC']];
+	}
 
 	get settOrder(): string[] {
 		return [
