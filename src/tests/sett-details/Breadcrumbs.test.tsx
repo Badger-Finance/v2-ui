313ab735722b2876f9d0b23ecf6b643d3e692a14
Elio <eliobricenov@gmail.com>
2021-08-03 21:48:48 -0400

    test: add sample test to snapshots

--- src/tests/sett-details/Breadcrumbs.test.tsx
+++ src/tests/sett-details/Breadcrumbs.test.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { checkSnapshot } from 'tests/utils/snapshots';
 import { Breadcrumb } from '../../components-v2/sett-detail/Breadcrumb';
+import { SAMPLE_SETT } from './utils';
 
 describe('Breadcrumb', () => {
 	it('displays sett breadcrumb', () => {
-		checkSnapshot(<Breadcrumb />);
+		checkSnapshot(<Breadcrumb sett={SAMPLE_SETT} />);
 	});
 });
