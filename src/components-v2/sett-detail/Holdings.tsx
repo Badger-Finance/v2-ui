7bf214327bbd9895ad2fbdb355b63ee846357e3e
Elio <eliobricenov@gmail.com>
2021-08-04 22:11:28 -0400

    feat: wire holdings information to users account

--- src/components-v2/sett-detail/Holdings.tsx
+++ src/components-v2/sett-detail/Holdings.tsx
@@ -1,34 +1,65 @@
 import React from 'react';
 import { Grid } from '@material-ui/core';
 import { HoldingItem } from './holdings/HoldingItem';
+import { observer } from 'mobx-react-lite';
+import { StoreContext } from '../../mobx/store-context';
+import { SettBalance } from '../../mobx/model/setts/sett-balance';
+import { formatWithoutExtraZeros, numberWithCommas } from '../../mobx/utils/helpers';
+import { Skeleton } from '@material-ui/lab';
+import BigNumber from 'bignumber.js';
 
-export const Holdings = (): JSX.Element => {
-	return (
-		<Grid container spacing={1}>
-			<Grid item xs={12} sm>
-				<HoldingItem
-					name="Your Total"
-					logo="/assets/icons/slp-digg-wbtc.png"
-					amount="1.21345"
-					dollarAmount="~$75,213"
-				/>
+interface Props {
+	settBalance: SettBalance;
+}
+
+const displayUsdBalance = (value: BigNumber.Value) => `~${numberWithCommas(formatWithoutExtraZeros(value, 3))}$`;
+
+export const Holdings = observer(
+	({ settBalance }: Props): JSX.Element => {
+		const { prices } = React.useContext(StoreContext);
+
+		const { id, depositedBalance, earnedBalance } = settBalance;
+		const usdExchangeRate = prices.exchangeRates?.usd;
+		const settPrice = prices.getPrice(id);
+		const principle = depositedBalance - earnedBalance;
+
+		let depositedUsd;
+		let principleUsd;
+		let earnedUsd;
+
+		if (usdExchangeRate !== undefined && !settPrice.isZero()) {
+			depositedUsd = displayUsdBalance(settPrice.multipliedBy(depositedBalance).multipliedBy(usdExchangeRate));
+			earnedUsd = displayUsdBalance(settPrice.multipliedBy(earnedBalance).multipliedBy(usdExchangeRate));
+			principleUsd = displayUsdBalance(settPrice.multipliedBy(principle).multipliedBy(usdExchangeRate));
+		}
+
+		return (
+			<Grid container spacing={1}>
+				<Grid item xs={12} sm>
+					<HoldingItem
+						name="Your Total"
+						logo={`/assets/icons/${settBalance.asset.toLowerCase()}.png`}
+						amount={formatWithoutExtraZeros(depositedBalance, 3)}
+						dollarAmount={depositedUsd ?? <Skeleton width={30} />}
+					/>
+				</Grid>
+				<Grid item xs={12} sm>
+					<HoldingItem
+						name="Principle"
+						logo={`/assets/icons/${settBalance.asset.toLowerCase()}.png`}
+						amount={formatWithoutExtraZeros(principle, 3)}
+						dollarAmount={principleUsd ?? <Skeleton width={30} />}
+					/>
+				</Grid>
+				<Grid item xs={12} sm>
+					<HoldingItem
+						name="Earned"
+						logo={`/assets/icons/${settBalance.asset.toLowerCase()}.png`}
+						amount={formatWithoutExtraZeros(earnedBalance, 3)}
+						dollarAmount={earnedUsd ?? <Skeleton width={30} />}
+					/>
+				</Grid>
 			</Grid>
-			<Grid item xs={12} sm>
-				<HoldingItem
-					name="Principle"
-					logo="/assets/icons/slp-digg-wbtc.png"
-					amount="1.01312"
-					dollarAmount="~$70,123"
-				/>
-			</Grid>
-			<Grid item xs={12} sm>
-				<HoldingItem
-					name="Earned"
-					logo="/assets/icons/slp-digg-wbtc.png"
-					amount="0.21345"
-					dollarAmount="~$5,090"
-				/>
-			</Grid>
-		</Grid>
-	);
-};
+		);
+	},
+);
--- src/components-v2/sett-detail/MainContent.tsx
+++ src/components-v2/sett-detail/MainContent.tsx
@@ -7,6 +7,8 @@ import { Breadcrumb } from './Breadcrumb';
 import { ChartsCard } from './charts/ChartsCard';
 import { Holdings } from './Holdings';
 import { Sett } from '../../mobx/model/setts/sett';
+import { observer } from 'mobx-react-lite';
+import { StoreContext } from '../../mobx/store-context';
 
 const useStyles = makeStyles((theme) => ({
 	content: {
@@ -31,30 +33,43 @@ interface Props {
 	sett: Sett;
 }
 
-export const MainContent = ({ sett }: Props): JSX.Element => {
-	const classes = useStyles();
-	const canFitActionButtons = useMediaQuery(useTheme().breakpoints.up(680));
+export const MainContent = observer(
+	({ sett }: Props): JSX.Element => {
+		const {
+			user: { accountDetails },
+			wallet: { connectedAddress },
+		} = React.useContext(StoreContext);
 
-	return (
-		<Grid container className={classes.content}>
-			<Grid container className={classes.breadcrumbContainer}>
-				<Breadcrumb sett={sett} />
-			</Grid>
-			<Grid container className={classes.descriptionSection}>
-				<Description sett={sett} />
-				{canFitActionButtons && <ActionButtons />}
-			</Grid>
-			<Grid container className={classes.holdingsContainer}>
-				<Holdings />
-			</Grid>
-			<Grid container spacing={1}>
-				<Grid item xs={12} md={4} lg={3}>
-					<SpecsCard />
+		const classes = useStyles();
+		const isMediumSizeScreen = useMediaQuery(useTheme().breakpoints.up('sm'));
+		const showActionButtons = isMediumSizeScreen && !!connectedAddress;
+		const settBalance = accountDetails?.balances.find((settBalance) => settBalance.id === sett.vaultToken);
+
+		return (
+			<Grid container className={classes.content}>
+				<Grid container className={classes.breadcrumbContainer}>
+					<Breadcrumb sett={sett} />
 				</Grid>
-				<Grid item xs={12} md={8} lg={9}>
-					<ChartsCard />
+				<Grid container className={classes.descriptionSection}>
+					<Description sett={sett} />
+					{showActionButtons && <ActionButtons />}
+				</Grid>
+				{settBalance && (
+					<Grid container className={classes.holdingsContainer}>
+						<Holdings settBalance={settBalance} />
+					</Grid>
+				)}
+				<Grid container spacing={1}>
+					<Grid item xs={12} md={4} lg={3}>
+						<SpecsCard />
+					</Grid>
+					{isMediumSizeScreen && (
+						<Grid item xs={12} md={8} lg={9}>
+							<ChartsCard />
+						</Grid>
+					)}
 				</Grid>
 			</Grid>
-		</Grid>
-	);
-};
+		);
+	},
+);
