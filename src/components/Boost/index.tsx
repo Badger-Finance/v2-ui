5c9c8e71e2ec55e03a3e0b050ae2f5ad87c8717b
Colin Spence <udappia@gmail.com>
2021-08-04 18:08:23 -0600

    Adds guide link

--- src/components/Boost/index.tsx
+++ src/components/Boost/index.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import { observer } from 'mobx-react-lite';
 import { Grid, Link } from '@material-ui/core';
+import Alert from '@material-ui/lab/Alert';
+import BookOutlinedIcon from '@material-ui/icons/BookOutlined';
 import { makeStyles } from '@material-ui/core/styles';
 import { Optimizer } from './Optimizer';
 import PageHeader from '../../components-v2/common/PageHeader';
@@ -12,6 +14,17 @@ const useStyles = makeStyles((theme) => ({
 		[theme.breakpoints.down('xs')]: {
 			display: 'none',
 		},
+	},
+	userGuide: {
+		color: theme.palette.text.secondary,
+		marginTop: theme.spacing(2),
+		width: '100%',
+	},
+	userGuideIcon: {
+		color: theme.palette.primary.main,
+	},
+	userGuideLink: {
+		marginLeft: 3,
 	},
 }));
 
@@ -39,6 +52,16 @@ export const BoostOptimizer = observer(() => {
 				<Grid item xs={12}>
 					<Optimizer />
 				</Grid>
+				<Alert
+					className={classes.userGuide}
+					severity="info"
+					iconMapping={{ info: <BookOutlinedIcon fontSize="inherit" className={classes.userGuideIcon} /> }}
+				>
+					Not sure where to start? Check out the
+					<Link href="#" className={classes.userGuideLink}>
+						Boost Optimizer User Guide
+					</Link>
+				</Alert>
 			</Grid>
 		</LayoutContainer>
 	);
