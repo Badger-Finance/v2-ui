b2ef9c6fbf236efdbb7fd79082ad7176c8479059
mitche50 <mitche50@gmail.com>
2021-08-09 11:33:09 -0400

    chore: update strategy link

--- src/config/system/strategies.ts
+++ src/config/system/strategies.ts
@@ -255,9 +255,8 @@ export const getStrategies = (network?: string | null): StrategyNetworkConfig =>
 						['DAO Performance Fee']: new BigNumber(2000),
 						['Withdraw Fee']: new BigNumber(20),
 					},
-					// TODO: Update this strategy link
 					strategyLink:
-						'https://badgerwiki.notion.site/Strategies-7bf5b27a451242538f02855ca5aaf4e4#f03b01a576d241aa9d9cee153876c976',
+						'https://badgerwiki.notion.site/Strategies-7bf5b27a451242538f02855ca5aaf4e4#d5806054c232432e8e8a1d75ae329bf8',
 				},
 				[deploy.sett_system.vaults['native.cvxCrv']]: {
 					name: '',
