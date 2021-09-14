95283f7f9a72b5022886d1fd06824468b10c619f
jintao <hellojintao@protonmail.com>
2021-09-14 14:56:20 -0400

    chore: fix divider

--- src/components-v2/sett-detail/specs/Tokens.tsx
+++ src/components-v2/sett-detail/specs/Tokens.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
-import { Grid, Typography } from '@material-ui/core';
+import { Grid, makeStyles, Typography } from '@material-ui/core';
 import { StyledDivider } from '../styled';
 import { Sett } from '../../../mobx/model/setts/sett';
 import { SettToken } from './SettToken';
+
+const useStyles = makeStyles((theme) => ({
+	title: {
+		paddingBottom: theme.spacing(0.5),
+	},
+}));
 
 interface Props {
 	sett: Sett;
 }
 
 export const Tokens = ({ sett }: Props): JSX.Element => {
+	const classes = useStyles();
 	return (
 		<Grid container>
-			<Typography>Tokens</Typography>
+			<Typography className={classes.title}>Tokens</Typography>
 			<StyledDivider />
 			<Grid container>
 				{sett.tokens.map((token, index) => (
