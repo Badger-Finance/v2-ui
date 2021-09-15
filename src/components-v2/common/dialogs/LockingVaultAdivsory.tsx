d8cfab4f0b0840491626aba4847b27718a2bf00e
jintao <hellojintao@protonmail.com>
2021-09-15 15:30:43 -0400

    chore: add click acknowledge warning

--- src/components-v2/common/dialogs/LockingVaultAdivsory.tsx
+++ src/components-v2/common/dialogs/LockingVaultAdivsory.tsx
@@ -1,11 +1,21 @@
+import { Link, makeStyles } from '@material-ui/core';
 import React from 'react';
 import GenericVaultAdvisory from './GenericVaulAdvisory';
+
+const useStyles = makeStyles(() => ({
+	linkContainer: {
+		display: 'flex',
+		justifyContent: 'center',
+		fontSize: '0.95rem',
+	},
+}));
 
 interface Props {
 	accept: () => void;
 }
 
 const LockingVaultAdvisory = ({ accept }: Props): JSX.Element => {
+	const classes = useStyles();
 	return (
 		<GenericVaultAdvisory accept={accept}>
 			<p>This vault locks Convex in a staking contract that is 16 weeks long.</p>
@@ -14,6 +24,19 @@ const LockingVaultAdvisory = ({ accept }: Props): JSX.Element => {
 				available and will fluctuate with deposits and withdraws from the vault as well as locks and unlocks
 				from the contract.
 			</p>
+			<p>
+				Any vault withdraws will have an opportunity to utilize the freshly deposited liquidity before it gets
+				locked on a first-come-first-serve basis.
+			</p>
+			<div className={classes.linkContainer}>
+				<Link
+					target="_blank"
+					rel="noreferrer"
+					href="https://screeching-panther-db4.notion.site/blCVX-Locked-Convex-4eabc8a3ef6b4df39d66cc98d2c1c7fc"
+				>
+					Learn More
+				</Link>
+			</div>
 		</GenericVaultAdvisory>
 	);
 };
