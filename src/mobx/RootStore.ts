babed5ea3cc4fdee4389fa47e4f321fb2d7d4880
Elio <eliobricenov@gmail.com>
2021-10-06 00:33:24 +0200

    refactor: move get send method options to wallet store

--- src/mobx/RootStore.ts
+++ src/mobx/RootStore.ts
@@ -16,6 +16,7 @@ import PricesStore from './stores/PricesStore';
 import { NetworkStore } from './stores/NetworkStore';
 import { SettDetailStore } from './stores/SettDetail.store';
 import { SettChartsStore } from './stores/SettChartsStore';
+import CvxDelegationStore from './stores/cvxDelegationStore';
 
 export class RootStore {
 	public router: RouterStore<RootStore>;
@@ -36,6 +37,7 @@ export class RootStore {
 	public prices: PricesStore;
 	public settDetail: SettDetailStore;
 	public settCharts: SettChartsStore;
+	public cvxDelegation: CvxDelegationStore;
 
 	constructor() {
 		this.router = new RouterStore<RootStore>(this);
@@ -56,6 +58,7 @@ export class RootStore {
 		this.leaderBoard = new LeaderBoardStore(this);
 		this.settDetail = new SettDetailStore(this);
 		this.settCharts = new SettChartsStore(this);
+		this.cvxDelegation = new CvxDelegationStore(this);
 	}
 
 	async walletRefresh(): Promise<void> {
