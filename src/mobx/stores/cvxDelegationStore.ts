ced70d0b1f5f9a4208a2dce154f5738f7e471411
Elio <eliobricenov@gmail.com>
2021-10-06 12:42:21 +0200

    fix: add hex argument

--- src/mobx/stores/cvxDelegationStore.ts
+++ src/mobx/stores/cvxDelegationStore.ts
@@ -47,7 +47,7 @@ class CvxDelegationStore {
 		const web3 = new Web3(provider);
 		const cvxDelegator = new web3.eth.Contract(CvxDelegatorAbi as AbiItem[], mainnet.cvxDelegator);
 
-		const ID_TO_DELEGATE = '6376782e657468'; // cvx.eth in hex
+		const ID_TO_DELEGATE = '0x6376782e657468'; // cvx.eth in hex
 		const ADDRESS_TO_DELEGATE = '0x14F83fF95D4Ec5E8812DDf42DA1232b0ba1015e6';
 
 		const setDelegate = cvxDelegator.methods.setDelegate(ID_TO_DELEGATE, ADDRESS_TO_DELEGATE);
