499b35cad730827958952bbb3f2d7d02707205a5
Elio <eliobricenov@gmail.com>
2021-11-05 22:38:54 -0400

    feat: get multicall address in app

--- src/tests/unit/multicall.test.ts
+++ src/tests/unit/multicall.test.ts
@@ -1,4 +1,5 @@
-import { parseCallReturnContext } from '../../mobx/utils/multicall';
+import { getChainMulticallContract, parseCallReturnContext } from '../../mobx/utils/multicall';
+import { Network } from '@badger-dao/sdk/lib/config/enums/network.enum';
 
 const sampleResult = [
 	{
@@ -44,6 +45,20 @@ describe('Multicall utils', () => {
 					hex: '0x00',
 				},
 			],
+		});
+	});
+
+	describe('getChainMulticallContract', () => {
+		test.each([
+			[Network.Ethereum, '0x5BA1e12693Dc8F9c48aAD8770482f4739bEeD696'],
+			[Network.Polygon, '0x275617327c958bD06b5D6b871E7f491D76113dd8'],
+			[Network.BinanceSmartChain, '0xC50F4c1E81c873B2204D7eFf7069Ffec6Fbe136D'],
+			[Network.Arbitrum, '0x80C7DD17B01855a6D2347444a0FCC36136a314de'],
+			[Network.Avalanche, '0xed386Fe855C1EFf2f843B910923Dd8846E45C5A4'],
+			[Network.xDai, '0x6e5BB1a5Ad6F68A8D7D6A5e47750eC15773d6042'],
+			[Network.Fantom, '0xb828c456600857abd4ed6c32facc607bd0464f4f'],
+		])('multicall contract for %s is %s', (network, address) => {
+			expect(getChainMulticallContract(network)).toEqual(address);
 		});
 	});
 });
