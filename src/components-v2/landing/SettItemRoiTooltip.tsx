b4eb4dc3b2cdcc78552bc6bc53be56267502a44e
jintao <hellojintao@protonmail.com>
2021-11-17 21:32:23 -0500

    chore: show min apr for no user

--- src/components-v2/landing/SettItemRoiTooltip.tsx
+++ src/components-v2/landing/SettItemRoiTooltip.tsx
@@ -1,19 +1,31 @@
 import { Sett } from '@badger-dao/sdk';
-import React from 'react';
+import { observer } from 'mobx-react-lite';
+import { StoreContext } from 'mobx/store-context';
+import React, { useContext } from 'react';
 
 interface Props {
 	sett: Sett;
 	multiplier?: number;
 }
 
-export const SettItemRoiTooltip = ({ sett, multiplier }: Props): JSX.Element => {
-	return (
-		<>
-			{sett.sources.map((source) => {
-				const sourceApr = source.boostable ? source.apr * (multiplier ?? 1) : source.apr;
-				const apr = `${sourceApr.toFixed(2)}% ${source.name}`;
-				return <div key={source.name}>{apr}</div>;
-			})}
-		</>
-	);
-};
+const SettItemRoiTooltip = observer(
+	({ sett, multiplier }: Props): JSX.Element => {
+		const { onboard } = useContext(StoreContext);
+		return (
+			<>
+				{sett.sources.map((source) => {
+					let sourceApr;
+					if (onboard.isActive()) {
+						sourceApr = source.boostable ? source.apr * (multiplier ?? 1) : source.apr;
+					} else {
+						sourceApr = source.minApr;
+					}
+					const apr = `${sourceApr.toFixed(2)}% ${source.name}`;
+					return <div key={source.name}>{apr}</div>;
+				})}
+			</>
+		);
+	},
+);
+
+export default SettItemRoiTooltip;
