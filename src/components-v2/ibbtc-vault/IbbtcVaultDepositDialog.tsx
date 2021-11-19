505ff2156fdaf5186601e20c82782915c2a4dde4
Elio <eliobricenov@gmail.com>
2021-11-19 18:39:59 -0400

    feat: add rest of deposit functionality

--- src/components-v2/ibbtc-vault/IbbtcVaultDepositDialog.tsx
+++ src/components-v2/ibbtc-vault/IbbtcVaultDepositDialog.tsx
@@ -16,6 +16,8 @@ import {
 	IconButton,
 	Radio,
 	RadioGroup,
+	Tab,
+	Tabs,
 	Typography,
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
@@ -33,10 +35,15 @@ import { Loader } from '../../components/Loader';
 import SlippageMessage from './SlippageMessage';
 import { debounce } from '../../utils/componentHelpers';
 import { ReportProblem } from '@material-ui/icons';
+import { BalanceNamespace } from '../../web3/config/namespaces';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
 		maxWidth: 680,
+	},
+	tab: {
+		textTransform: 'none',
 	},
 	avatar: {
 		display: 'inline-block',
@@ -62,7 +69,7 @@ const useStyles = makeStyles((theme) => ({
 		margin: theme.spacing(1, 0),
 	},
 	inputsContainer: {
-		marginTop: theme.spacing(1),
+		marginTop: theme.spacing(2),
 	},
 	depositButton: {
 		marginTop: theme.spacing(1),
@@ -85,22 +92,72 @@ const useStyles = makeStyles((theme) => ({
 		color: '#D97706',
 		marginRight: theme.spacing(1),
 	},
+	depositContent: {
+		marginTop: theme.spacing(2),
+	},
+	estimations: {},
 }));
+
+enum DepositMode {
+	tokens = 'tokens',
+	lpToken = 'lp-token',
+}
 
 const ibbtcVaultPeakAddress = mainnetDeploy.ibbtcVaultZap;
 
 const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX.Element => {
 	const classes = useStyles();
 	const store = useContext(StoreContext);
-	const { user, uiState, contracts, onboard } = store;
+	const { contracts, network, onboard, setts, uiState, user } = store;
+
+	const lpSett = setts.getSett(mainnetDeploy.sett_system.vaults['native.ibbtcCrv']);
+	const lpBadgerSett = network.network.setts.find(({ vaultToken }) => vaultToken.address === lpSett?.settToken);
+	const userLpTokenBalance = lpBadgerSett ? user.getBalance(BalanceNamespace.Sett, lpBadgerSett) : undefined;
+	const userHasLpTokenBalance = userLpTokenBalance?.tokenBalance.gt(0);
+
+	const [mode, setMode] = useState(userHasLpTokenBalance ? DepositMode.lpToken : DepositMode.tokens);
 	const [slippageRevertProtected, setSlippageRevertProtected] = useState(false);
 	const [slippage, setSlippage] = useState(0.3);
 	const [expectedSlippage, setExpectedSlippage] = useState<BigNumber>();
 	const [depositOptions, setDepositOptions] = useState<TokenBalance[]>([]);
-	const [depositBalances, setDepositBalances] = useState<TokenBalance[]>([]);
+	const [multiTokenDepositBalances, setMultiTokenDepositBalances] = useState<TokenBalance[]>([]);
+	const [lpTokenDepositBalance, setLpTokenDepositBalance] = useState<TokenBalance>();
+	const [expectedPoolTokens, setExpectedPoolTokens] = useState<TokenBalance>();
+	const [minPoolTokens, setMinPoolTokens] = useState<TokenBalance>();
 
-	const areOptionAvailable = Object.keys(user.tokenBalances).length > 0;
-	const totalDeposit = depositBalances.reduce((total, balance) => total.plus(balance.tokenBalance), new BigNumber(0));
+	const areOptionAvailable = Object.keys(user.tokenBalances).length > 0 && lpBadgerSett;
+	const totalDeposit = multiTokenDepositBalances.reduce(
+		(total, balance) => total.plus(balance.tokenBalance),
+		new BigNumber(0),
+	);
+
+	const multiTokenDisabled = totalDeposit.isZero() || slippageRevertProtected;
+	const lpTokenDisabled = !lpTokenDepositBalance || lpTokenDepositBalance.tokenBalance.isZero();
+
+	const resetCalculatedInformation = useCallback(() => {
+		setMultiTokenDepositBalances(depositOptions);
+		setSlippageRevertProtected(false);
+		setExpectedSlippage(undefined);
+		setExpectedPoolTokens(undefined);
+		setMinPoolTokens(undefined);
+	}, [depositOptions]);
+
+	const getCalculations = useCallback(
+		async (balances: TokenBalance[]): Promise<BigNumber[]> => {
+			const web3 = new Web3(onboard.wallet?.provider);
+			const ibbtcVaultPeak = new web3.eth.Contract(IbbtcVaultZapAbi as AbiItem[], ibbtcVaultPeakAddress);
+
+			const depositAmounts = balances.map((balance) => toHex(balance.tokenBalance));
+
+			const [calculatedMint, expectedAmount] = await Promise.all([
+				new BigNumber(await ibbtcVaultPeak.methods.calcMint(depositAmounts, false).call()),
+				new BigNumber(await ibbtcVaultPeak.methods.expectedAmount(depositAmounts).call()),
+			]);
+
+			return [calculatedMint, expectedAmount];
+		},
+		[onboard],
+	);
 
 	const handleClosing = () => {
 		setSlippageRevertProtected(false);
@@ -108,11 +165,29 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 		onClose();
 	};
 
+	const handleModeChange = (newMode: DepositMode) => {
+		resetCalculatedInformation();
+		setMode(newMode);
+	};
+
+	const handleSlippageChange = async (newSlippage: number) => {
+		if (!userLpTokenBalance) {
+			return;
+		}
+
+		const { 1: expectedAmount } = await getCalculations(multiTokenDepositBalances);
+		const minOut = expectedAmount.multipliedBy(1 - newSlippage / 100);
+
+		setMinPoolTokens(TokenBalance.fromBigNumber(userLpTokenBalance, minOut));
+		setExpectedPoolTokens(TokenBalance.fromBigNumber(userLpTokenBalance, expectedAmount));
+		setSlippage(newSlippage);
+	};
+
 	// reason: the plugin does not recognize the dependency inside the debounce function
 	// eslint-disable-next-line react-hooks/exhaustive-deps
-	const handleChange = useCallback(
+	const handleDepositBalanceChange = useCallback(
 		debounce(200, async (tokenBalance: TokenBalance, index: number) => {
-			const balances = [...depositBalances];
+			const balances = [...multiTokenDepositBalances];
 			balances[index] = tokenBalance;
 
 			const totalDeposit = balances.reduce(
@@ -121,40 +196,39 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 			);
 
 			if (totalDeposit.isZero()) {
-				setDepositBalances(balances);
-				setSlippageRevertProtected(false);
-				setExpectedSlippage(undefined);
+				resetCalculatedInformation();
 				return;
 			}
 
-			const web3 = new Web3(onboard.wallet?.provider);
-			const ibbtcVaultPeak = new web3.eth.Contract(IbbtcVaultZapAbi as AbiItem[], ibbtcVaultPeakAddress);
-			const depositAmounts = balances.map((balance) => toHex(balance.tokenBalance));
-
-			console.log(JSON.stringify(depositAmounts));
-
-			const [calculatedMint, expectedAmount] = await Promise.all([
-				new BigNumber(await ibbtcVaultPeak.methods.calcMint(depositAmounts, false).call()),
-				new BigNumber(await ibbtcVaultPeak.methods.expectedAmount(depositAmounts).call()),
-			]);
-
-			console.log('calculatedMint =>', calculatedMint.toString());
-			console.log('expectedAmount =>', expectedAmount.toString());
+			const [calculatedMint, expectedAmount] = await getCalculations(balances);
 
 			// formula is: slippage = [(expectedAmount - calculatedMint) * 100] / expectedAmount
 			const calculatedSlippage = expectedAmount.minus(calculatedMint).multipliedBy(100).dividedBy(expectedAmount);
 			const minOut = expectedAmount.multipliedBy(1 - slippage / 100);
 
+			if (userLpTokenBalance) {
+				setMinPoolTokens(TokenBalance.fromBigNumber(userLpTokenBalance, minOut));
+				setExpectedPoolTokens(TokenBalance.fromBigNumber(userLpTokenBalance, expectedAmount));
+			}
+
 			// this will protect users from submitting tx that will be reverted because of slippage
 			setSlippageRevertProtected(calculatedMint.isLessThan(minOut));
 			setExpectedSlippage(calculatedSlippage);
-			setDepositBalances(balances);
+			setMultiTokenDepositBalances(balances);
 		}),
-		[depositBalances, slippage],
+		[getCalculations, resetCalculatedInformation, userLpTokenBalance, multiTokenDepositBalances, slippage],
 	);
 
-	const handleDeposit = async () => {
-		const invalidBalance = depositBalances.find((depositBalance, index) => {
+	const handleLpTokenDeposit = async () => {
+		if (!lpTokenDepositBalance || !userLpTokenBalance || !lpSett || !lpBadgerSett) {
+			return;
+		}
+
+		await contracts.deposit(lpSett, lpBadgerSett, userLpTokenBalance, lpTokenDepositBalance);
+	};
+
+	const handleMultiTokenDeposit = async () => {
+		const invalidBalance = multiTokenDepositBalances.find((depositBalance, index) => {
 			const depositOption = depositOptions[index];
 			return depositBalance.tokenBalance.gt(depositOption.tokenBalance);
 		});
@@ -164,7 +238,7 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 			return;
 		}
 
-		for (const depositBalance of depositBalances) {
+		for (const depositBalance of multiTokenDepositBalances) {
 			const allowance = await contracts.getAllowance(depositBalance.token, ibbtcVaultPeakAddress);
 
 			if (allowance.tokenBalance.lt(depositBalance.tokenBalance)) {
@@ -175,7 +249,7 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 		const web3 = new Web3(onboard.wallet?.provider);
 		const ibbtcVaultPeak = new web3.eth.Contract(IbbtcVaultZapAbi as AbiItem[], ibbtcVaultPeakAddress);
 
-		const depositAmounts = depositBalances.map((balance) => toHex(balance.tokenBalance));
+		const depositAmounts = multiTokenDepositBalances.map((balance) => toHex(balance.tokenBalance));
 		const expectedAmount = await new BigNumber(await ibbtcVaultPeak.methods.expectedAmount(depositAmounts).call());
 		const minOut = expectedAmount.multipliedBy(1 - slippage / 100);
 
@@ -199,7 +273,7 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 		const wBTC = user.getTokenBalance(mainnetDeploy.tokens['wBTC']);
 		const ibbtc = user.getTokenBalance(mainnetDeploy.tokens['ibBTC']);
 		setDepositOptions([ibbtc, renBTC, wBTC, sBTC]);
-		setDepositBalances([ibbtc, renBTC, wBTC, sBTC]);
+		setMultiTokenDepositBalances([ibbtc, renBTC, wBTC, sBTC]);
 	}, [user, user.initialized]);
 
 	return (
@@ -224,44 +298,91 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 								<Typography variant="body1">Convex</Typography>
 							</Box>
 						</Grid>
-						<Grid item container direction="column" className={classes.inputsContainer}>
-							{depositOptions.map((tokenBalance, index) => (
-								<Grid item key={`${tokenBalance.token.address}_${index}`} className={classes.inputRow}>
-									<BalanceInput
-										tokenBalance={tokenBalance}
-										onChange={(change) => handleChange(change, index)}
-									/>
-								</Grid>
-							))}
-							<Grid
-								item
-								container
-								justify="space-between"
-								alignItems="center"
-								className={classes.inputRow}
+						<Grid item container className={classes.depositContent}>
+							<Tabs
+								value={mode}
+								textColor="primary"
+								indicatorColor="primary"
+								onChange={(e, newMode) => handleModeChange(newMode)}
+								aria-label="wrapped label tabs example"
 							>
-								<Typography variant="subtitle2">Slippage</Typography>
-								<RadioGroup
-									row
-									value={slippage}
-									onChange={(event) => setSlippage(Number(event.target.value))}
-								>
-									{[0.1, 0.3, 0.5, 1].map((slippageOption, index) => (
-										<FormControlLabel
-											key={`${slippageOption}_${index}`}
-											control={<Radio color="primary" />}
-											label={`${slippageOption}%`}
-											value={slippageOption}
-										/>
-									))}
-								</RadioGroup>
+								<Tab
+									className={classes.tab}
+									value={DepositMode.tokens}
+									label="ibBTC, renBTC, WBTC & sBTC"
+								/>
+								<Tab className={classes.tab} value={DepositMode.lpToken} label="LP Token" />
+							</Tabs>
+							<Grid container direction="column" className={classes.inputsContainer}>
+								{mode === DepositMode.tokens ? (
+									<>
+										{depositOptions.map((tokenBalance, index) => (
+											<Grid
+												item
+												key={`${tokenBalance.token.address}_${index}`}
+												className={classes.inputRow}
+											>
+												<BalanceInput
+													tokenBalance={tokenBalance}
+													onChange={(change) => handleDepositBalanceChange(change, index)}
+												/>
+											</Grid>
+										))}
+										<Grid
+											item
+											container
+											justify="space-between"
+											alignItems="center"
+											className={classes.inputRow}
+										>
+											<Typography variant="subtitle2">Slippage</Typography>
+											<RadioGroup
+												row
+												value={slippage}
+												onChange={(event) => handleSlippageChange(Number(event.target.value))}
+											>
+												{[0.1, 0.3, 0.5, 1].map((slippageOption, index) => (
+													<FormControlLabel
+														key={`${slippageOption}_${index}`}
+														control={<Radio color="primary" />}
+														label={`${slippageOption}%`}
+														value={slippageOption}
+													/>
+												))}
+											</RadioGroup>
+										</Grid>
+									</>
+								) : (
+									<Grid item className={classes.inputRow}>
+										{userLpTokenBalance && (
+											<BalanceInput
+												tokenBalance={userLpTokenBalance}
+												onChange={(change) => setLpTokenDepositBalance(change)}
+											/>
+										)}
+									</Grid>
+								)}
 							</Grid>
 						</Grid>
-						{expectedSlippage && (
-							<Grid item container alignItems="center" className={classes.inputRow}>
-								<SlippageMessage limitSlippage={slippage} calculatedSlippage={expectedSlippage} />
-							</Grid>
-						)}
+						<Grid item container direction="column" className={clsx(classes.inputRow, classes.estimations)}>
+							{expectedPoolTokens && (
+								<Grid item container justify="space-between">
+									<Typography variant="body2">Expected Pool Tokens Received:</Typography>
+									<Typography variant="body2">{expectedPoolTokens.balanceDisplay(4)}</Typography>
+								</Grid>
+							)}
+							{minPoolTokens && (
+								<Grid item container justify="space-between">
+									<Typography variant="body2">Min Pool tokens Received:</Typography>
+									<Typography variant="body2">{minPoolTokens.balanceDisplay(4)}</Typography>
+								</Grid>
+							)}
+							{expectedSlippage && (
+								<Grid item>
+									<SlippageMessage limitSlippage={slippage} calculatedSlippage={expectedSlippage} />
+								</Grid>
+							)}
+						</Grid>
 						{slippageRevertProtected && (
 							<Grid
 								item
@@ -296,8 +417,8 @@ const IbbtcVaultDepositDialog = ({ open = false, onClose }: SettModalProps): JSX
 					variant="contained"
 					color="primary"
 					className={classes.depositButton}
-					disabled={totalDeposit.isZero() || slippageRevertProtected}
-					onClick={handleDeposit}
+					disabled={mode === DepositMode.tokens ? multiTokenDisabled : lpTokenDisabled}
+					onClick={mode === DepositMode.tokens ? handleMultiTokenDeposit : handleLpTokenDeposit}
 				>
 					{slippageRevertProtected ? 'Slippage out of range' : 'Deposit'}
 				</Button>
