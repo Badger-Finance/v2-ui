87d27a25e2c981ebc48ea58c89a160284c94b288
Elio <eliobricenov@gmail.com>
2021-11-25 22:42:17 -0400

    styles: revamp announcement banner

---
+++ src/components-v2/common/Announcements.tsx
@@ -0,0 +1,73 @@
+import React, { useContext } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import { IconButton, Typography } from '@material-ui/core';
+import CloseIcon from '@material-ui/icons/Close';
+import { StoreContext } from '../../mobx/store-context';
+import { observer } from 'mobx-react-lite';
+
+const useStyles = makeStyles((theme) => ({
+	root: {
+		width: '100%',
+		background: '#6B6B6B',
+		padding: '15px 0',
+		whiteSpace: 'pre-wrap',
+	},
+	link: {
+		color: theme.palette.primary.main,
+		textDecoration: 'none',
+		fontWeight: 'bold',
+		marginLeft: theme.spacing(0.25),
+	},
+	closeButton: {
+		padding: 0,
+		fontSize: 14,
+		marginLeft: 12,
+		[theme.breakpoints.down('sm')]: {
+			position: 'absolute',
+			right: theme.spacing(2),
+		},
+	},
+	content: {
+		display: 'flex',
+		alignItems: 'center',
+		justifyContent: 'center',
+		width: '100%',
+		[theme.breakpoints.down('xs')]: {
+			width: '60%',
+			margin: 'auto',
+			textAlign: 'center',
+		},
+	},
+}));
+
+const Announcements = (): JSX.Element | null => {
+	const { uiState } = useContext(StoreContext);
+	const classes = useStyles();
+
+	if (!uiState.shouldShowNotification) {
+		return null;
+	}
+
+	return (
+		<div id="app-notification" className={classes.root}>
+			<div className={classes.content}>
+				<Typography variant="body2">
+					{'Badger Boost Power has been implemented. '}{' '}
+					<a
+						href="https://badger.com/news/single-chain-boost"
+						rel="noreferrer"
+						target="_blank"
+						className={classes.link}
+					>
+						Learn More
+					</a>
+				</Typography>
+				<IconButton className={classes.closeButton} onClick={() => uiState.closeNotification()}>
+					<CloseIcon />
+				</IconButton>
+			</div>
+		</div>
+	);
+};
+
+export default observer(Announcements);
