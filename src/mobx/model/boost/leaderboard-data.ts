8b78e28975dcf7e39267a59c43749de279773c21
jintao <hellojintao@protonmail.com>
2021-11-30 16:58:42 -0500

    fix: remove unused file

--- src/mobx/model/boost/leaderboard-data.ts
+++
@@ -1,9 +0,0 @@
-import { LeaderBoardBadger } from './leader-board-badger';
-
-export interface LeaderBoardData {
-	data: LeaderBoardBadger[];
-	page: number;
-	size: number;
-	count: number;
-	maxPage: number;
-}
