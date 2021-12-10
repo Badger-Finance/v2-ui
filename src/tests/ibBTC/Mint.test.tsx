b5ba5a79078cf09585288989351722683b0c2edc
Elio <eliobricenov@gmail.com>
2021-12-10 19:34:33 -0400

    tests: enhance mint and redeem test suites

--- src/tests/ibBTC/Mint.test.tsx
+++ src/tests/ibBTC/Mint.test.tsx
@@ -9,6 +9,8 @@ import { IbbtcOptionToken } from '../../mobx/model/tokens/ibbtc-option-token';
 import { Snackbar } from '../../components/Snackbar';
 import Header from '../../components/Header';
 import IbBTCStore from 'mobx/stores/ibBTCStore';
+import { within } from '@testing-library/react';
+import BigNumber from 'bignumber.js';
 
 const tokensConfig = addresses.mainnet.tokens;
 const mockIbBTC = new IbbtcOptionToken(store, tokensConfig['ibBTC']);
@@ -20,9 +22,12 @@ const mockTokens = [
 
 describe('ibBTC Mint', () => {
 	beforeEach(() => {
+		store.onboard.address = '0x1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a';
+
 		store.ibBTCStore.ibBTC = mockIbBTC;
 		store.ibBTCStore.tokens = mockTokens;
 		store.ibBTCStore.tokens[0].balance = store.ibBTCStore.tokens[0].scale('5');
+		store.ibBTCStore.tokens[1].balance = store.ibBTCStore.tokens[1].scale('10');
 		store.ibBTCStore.ibBTC.balance = mockIbBTC.scale('10');
 		/* eslint-disable-next-line @typescript-eslint/no-unused-vars */
 		jest.spyOn(IbBTCStore.prototype, 'calcMintAmount').mockImplementation(async (_token, _amount) => ({
@@ -73,7 +78,29 @@ describe('ibBTC Mint', () => {
 		expect(container).toMatchSnapshot();
 	});
 
+	it('can change token', async () => {
+		const { container } = customRender(
+			<StoreProvider value={store}>
+				<Mint />
+			</StoreProvider>,
+		);
+
+		fireEvent.click(screen.getByRole('button', { name: 'token options' }));
+
+		const newTokenOption = within(screen.getByRole('list', { name: 'token options list' })).getByAltText(
+			`${store.ibBTCStore.tokens[1].name} icon`,
+		);
+
+		fireEvent.click(newTokenOption);
+
+		await screen.findByText(store.ibBTCStore.tokens[1].name);
+
+		expect(container).toMatchSnapshot();
+	});
+
 	it('handles not connected wallet', () => {
+		store.onboard.address = undefined;
+
 		customRender(
 			<StoreProvider value={store}>
 				<Mint />
@@ -85,10 +112,6 @@ describe('ibBTC Mint', () => {
 	});
 
 	it('handles empty balance', async () => {
-		jest.useFakeTimers();
-
-		store.onboard.address = '0x1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a';
-
 		customRender(
 			<StoreProvider value={store}>
 				<Snackbar>
@@ -100,12 +123,59 @@ describe('ibBTC Mint', () => {
 
 		fireEvent.change(screen.getByRole('textbox'), { target: { value: '12' } });
 
-		jest.advanceTimersByTime(1000);
-
 		await screen.findByText('11.988000 ibBTC');
 
 		fireEvent.click(screen.getByRole('button', { name: /mint/i }));
 
 		expect(screen.getByText('You have insufficient balance of bcrvRenBTC')).toBeInTheDocument();
+	});
+
+	it('executes calcMint with correct params', async () => {
+		const calcMintSpy = jest.spyOn(IbBTCStore.prototype, 'calcMintAmount');
+
+		customRender(
+			<StoreProvider value={store}>
+				<Snackbar>
+					<Header />
+					<Mint />
+				</Snackbar>
+			</StoreProvider>,
+		);
+
+		fireEvent.change(screen.getByRole('textbox'), { target: { value: '0.1' } });
+
+		await screen.findByText('11.988000 ibBTC');
+
+		expect(calcMintSpy).toHaveBeenNthCalledWith(
+			1,
+			store.ibBTCStore.tokens[0],
+			store.ibBTCStore.tokens[0].scale('0.1'),
+		);
+	});
+
+	it('executes mint with correct params', async () => {
+		const mintSpy = jest.spyOn(IbBTCStore.prototype, 'mint');
+
+		customRender(
+			<StoreProvider value={store}>
+				<Snackbar>
+					<Header />
+					<Mint />
+				</Snackbar>
+			</StoreProvider>,
+		);
+
+		fireEvent.change(screen.getByRole('textbox'), { target: { value: '0.1' } });
+
+		await screen.findByText('11.988000 ibBTC');
+
+		fireEvent.click(screen.getByRole('button', { name: /mint/i }));
+
+		expect(mintSpy).toHaveBeenNthCalledWith(
+			1,
+			store.ibBTCStore.tokens[0],
+			store.ibBTCStore.tokens[0].scale('0.1'),
+			new BigNumber('1'),
+		);
 	});
 });
