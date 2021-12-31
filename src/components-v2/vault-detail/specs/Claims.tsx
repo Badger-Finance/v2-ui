f1ed68ed90471e1ef393c00c17597921859a8db9
elio <eliobricenov@gmail.com>
2021-12-31 13:56:50 -0400

    chore: use singular in verbiage

--- src/components-v2/vault-detail/specs/Claims.tsx
+++ src/components-v2/vault-detail/specs/Claims.tsx
@@ -83,8 +83,8 @@ export const Claims = observer((): JSX.Element => {
 			<StyledDivider />
 			<Typography className={classes.frequencyDetail} variant="caption" color="textSecondary">
 				{isDelayed
-					? 'This Setts’s rewards are currently taking longer than usual.'
-					: 'This Setts’s rewards are currently being processed.'}
+					? 'This Sett’s rewards are currently taking longer than usual.'
+					: 'This Sett’s rewards are currently being processed.'}
 			</Typography>
 			<Link
 				className={classes.infoLink}
