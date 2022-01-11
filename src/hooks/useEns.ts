4fa5b9624871109b842bce513827ec9330bd7e57
jintao <hellojintao@protonmail.com>
2022-01-11 13:46:14 -0500

    chore: add debug logging as warn

--- src/hooks/useEns.ts
+++ src/hooks/useEns.ts
@@ -1,30 +1,33 @@
-import { ethers } from "ethers";
-import { StoreContext } from "mobx/store-context";
-import { useContext, useEffect, useState } from "react";
+import { DEBUG } from 'config/environment';
+import { ethers } from 'ethers';
+import { StoreContext } from 'mobx/store-context';
+import { useContext, useEffect, useState } from 'react';
 
 const useENS = (address?: string) => {
-  const store = useContext(StoreContext);
-  const { provider } = store.onboard;
-  const [ensName, setENSName] = useState<string | null>(null);
-  const [ensAvatar, setENSAvatar] = useState<string | null>(null);
+	const store = useContext(StoreContext);
+	const { provider } = store.onboard;
+	const [ensName, setENSName] = useState<string | null>(null);
+	const [ensAvatar, setENSAvatar] = useState<string | null>(null);
 
-  useEffect(() => {
-    const resolveENS = async () => {
-      if (provider && address && ethers.utils.isAddress(address)) {
-        try {
-          let ensName = await provider.lookupAddress(address);
-          let avatar = ensName ? await provider.getAvatar(ensName) : null;
-          setENSName(ensName);
-          setENSAvatar(avatar);
-        } catch (e) {
-          console.log("e", e);
-        }
-      }
-    };
-    resolveENS();
-  }, [provider, address]);
+	useEffect(() => {
+		const resolveENS = async () => {
+			if (provider && address && ethers.utils.isAddress(address)) {
+				try {
+					let ensName = await provider.lookupAddress(address);
+					let avatar = ensName ? await provider.getAvatar(ensName) : null;
+					setENSName(ensName);
+					setENSAvatar(avatar);
+				} catch (e) {
+          if (DEBUG) {
+					  console.warn('ENS lookup encountered an error', e);
+          }
+				}
+			}
+		};
+		resolveENS();
+	}, [provider, address]);
 
-  return { ensName, ensAvatar };
+	return { ensName, ensAvatar };
 };
 
 export default useENS;
