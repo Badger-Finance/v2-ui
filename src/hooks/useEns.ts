30848937bc903dbad1b0e28176ad0eb509df7ddb
jintao <hellojintao@protonmail.com>
2022-01-11 13:40:23 -0500

    feat: add use-ens

---
+++ src/hooks/useEns.ts
@@ -0,0 +1,30 @@
+import { ethers } from "ethers";
+import { StoreContext } from "mobx/store-context";
+import { useContext, useEffect, useState } from "react";
+
+const useENS = (address?: string) => {
+  const store = useContext(StoreContext);
+  const { provider } = store.onboard;
+  const [ensName, setENSName] = useState<string | null>(null);
+  const [ensAvatar, setENSAvatar] = useState<string | null>(null);
+
+  useEffect(() => {
+    const resolveENS = async () => {
+      if (provider && address && ethers.utils.isAddress(address)) {
+        try {
+          let ensName = await provider.lookupAddress(address);
+          let avatar = ensName ? await provider.getAvatar(ensName) : null;
+          setENSName(ensName);
+          setENSAvatar(avatar);
+        } catch (e) {
+          console.log("e", e);
+        }
+      }
+    };
+    resolveENS();
+  }, [provider, address]);
+
+  return { ensName, ensAvatar };
+};
+
+export default useENS;
