7dfc011d7bc187c54ed6f2a09913d73493de5005
jintao <hellojintao@protonmail.com>
2022-01-20 15:01:30 -0500

    chore: adhere to 10k block limit

--- src/mobx/utils/diggHelpers.ts
+++ src/mobx/utils/diggHelpers.ts
@@ -98,11 +98,8 @@ export const getRebaseLogs = async (provider: provider, network: NetworkModel): 
 	const policy = rebaseConfig.digg[1];
 	const contractInstance = new web3.eth.Contract(policy.abi as AbiItem[], policy.contractAddress);
 	const currentBlock = await web3.eth.getBlockNumber();
-	// remove one month of blocks to get start block
-	// 30 * 24 * 60 * 60 - number of seconds in a month, div 13 for seconds per block
-	const startBlock = currentBlock - (30 * 24 * 60 * 60) / 13;
 	const events = await contractInstance.getPastEvents('LogRebase', {
-		fromBlock: startBlock.toFixed(),
+		fromBlock: (currentBlock - 10_000).toFixed(),
 		toBlock: (currentBlock - 1).toFixed(),
 	});
 	return events.length ? events[events.length - 1].returnValues : null;
