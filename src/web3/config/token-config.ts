4ed1f45fa677a05b58287a0fe5d9c6df13eb42c5
elio <eliobricenov@gmail.com>
2022-02-08 18:24:10 -0400

    fix: don't use comma operator

--- src/web3/config/token-config.ts
+++ src/web3/config/token-config.ts
@@ -37,5 +37,8 @@ export function toRecord<
 	T extends { [K in keyof T]: string | number | symbol }, // added constraint
 	K extends keyof T,
 >(array: T[], selector: K): Record<T[K], T> {
-	return array.reduce((acc, item) => ((acc[item[selector]] = item), acc), {} as Record<T[K], T>);
+	return array.reduce((acc, item) => {
+		acc[item[selector]] = item;
+		return acc;
+	}, {} as Record<T[K], T>);
 }
