259a2fa97d1958507388bd40122d43836fdadd58
elio <eliobricenov@gmail.com>
2022-02-15 14:44:17 -0400

    tests: remove flag in mocks

--- src/tests/sett-details/SpecsCard.test.tsx
+++ src/tests/sett-details/SpecsCard.test.tsx
@@ -2,17 +2,14 @@ import SpecsCard from 'components-v2/vault-detail/specs/SpecsCard';
 import React from 'react';
 import { checkSnapshot } from 'tests/utils/snapshots';
 import { SAMPLE_BADGER_SETT, SAMPLE_SETT } from '../utils/samples';
-import { FLAGS } from '../../config/environment';
 import VaultStore from '../../mobx/stores/VaultStore';
 
 describe('Specs Section', () => {
 	it('displays sett information', () => {
-		if (FLAGS.SDK_INTEGRATION_ENABLED) {
-			jest.spyOn(VaultStore.prototype, 'getVaultDefinition').mockReturnValue({
-				depositToken: { address: SAMPLE_SETT.underlyingToken, decimals: 18 },
-				vaultToken: { address: SAMPLE_SETT.vaultToken, decimals: 18 },
-			});
-		}
+		jest.spyOn(VaultStore.prototype, 'getVaultDefinition').mockReturnValue({
+			depositToken: { address: SAMPLE_SETT.underlyingToken, decimals: 18 },
+			vaultToken: { address: SAMPLE_SETT.vaultToken, decimals: 18 },
+		});
 		checkSnapshot(<SpecsCard vault={SAMPLE_SETT} badgerVault={SAMPLE_BADGER_SETT} />);
 	});
 });
