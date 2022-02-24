3ab41be737aee9852baa810604d4dcfc11059b99
elio <eliobricenov@gmail.com>
2022-02-24 14:04:23 -0400

    refactor: use component for token display
    closes #1690

--- src/components-v2/common/WalletDrawer.tsx
+++ src/components-v2/common/WalletDrawer.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useState } from 'react';
 import { observer } from 'mobx-react-lite';
 import { StoreContext } from '../../mobx/store-context';
-import { Box, Button, Drawer, Grid, IconButton, makeStyles, Typography, useTheme } from '@material-ui/core';
+import { Button, Drawer, Grid, IconButton, makeStyles, Typography, useTheme } from '@material-ui/core';
 import CloseIcon from '@material-ui/icons/Close';
 import useENS from '../../hooks/useEns';
 import { shortenAddress } from '../../utils/componentHelpers';
@@ -9,6 +9,8 @@ import CurrencyDisplay from './CurrencyDisplay';
 import { inCurrency } from '../../mobx/utils/helpers';
 import copy from 'copy-to-clipboard';
 import BigNumber from 'bignumber.js';
+import WalletTokenBalance from './WalletTokenBalance';
+import clsx from 'clsx';
 
 const useStyles = makeStyles((theme) => ({
 	root: {
@@ -23,10 +25,12 @@ const useStyles = makeStyles((theme) => ({
 		fontWeight: 700,
 	},
 	balance: {
+		color: 'rgba(255, 255, 255, 0.87)',
 		fontSize: 20,
 	},
 	disconnectWalletText: {
-		color: '#FB0505',
+		textTransform: 'uppercase',
+		color: '#91CDFF',
 		fontWeight: 700,
 	},
 	titleRow: {
@@ -35,25 +39,8 @@ const useStyles = makeStyles((theme) => ({
 	addressRow: {
 		marginBottom: theme.spacing(2),
 	},
-	tokenNameAndIcon: {
-		display: 'flex',
-		alignItems: 'center',
-	},
-	tokenName: {
-		fontSize: 16,
-	},
-	icon: {
-		width: 25,
-		marginRight: 15,
-	},
-	tokenBalance: {
-		marginBottom: theme.spacing(3),
-	},
 	balancesList: {
 		marginTop: 32,
-	},
-	balanceDisplayValue: {
-		marginTop: theme.spacing(-0.5),
 	},
 	copiedMessage: {
 		background: '#66BB6A',
@@ -71,7 +58,10 @@ const useStyles = makeStyles((theme) => ({
 		marginLeft: theme.spacing(4),
 	},
 	disconnectWalletIcon: {
-		marginRight: theme.spacing(3),
+		marginRight: theme.spacing(1),
+	},
+	address: {
+		textTransform: 'uppercase',
 	},
 }));
 
@@ -126,7 +116,12 @@ const WalletDrawer = (): JSX.Element | null => {
 						</IconButton>
 					</Grid>
 					<Grid item container alignItems="center" className={classes.addressRow}>
-						<Typography variant="subtitle2" display="inline">
+						<Typography
+							className={clsx(!ensName && classes.address)}
+							variant="subtitle2"
+							color="textSecondary"
+							display="inline"
+						>
 							{ensName || shortenAddress(onboard.address)}
 						</Typography>
 						<IconButton
@@ -150,32 +145,7 @@ const WalletDrawer = (): JSX.Element | null => {
 					</Grid>
 					<Grid item container className={classes.balancesList}>
 						{sortedBalances.map((tokenBalance) => (
-							<Grid container className={classes.tokenBalance} key={tokenBalance.token.address}>
-								<Grid item container justifyContent="space-between" alignItems="center">
-									<div className={classes.tokenNameAndIcon}>
-										<img
-											className={classes.icon}
-											// small hack to allow rem tokens to share icons with their badger counterparts
-											src={`/assets/icons/${tokenBalance.token.symbol
-												.replace('rem', '')
-												.toLowerCase()
-												.trim()}.png`}
-											alt={`${tokenBalance.token.name} icon`}
-										/>
-										<Typography variant="body1" display="inline" className={classes.tokenName}>
-											{tokenBalance.token.symbol}
-										</Typography>
-									</div>
-									<Box display="inline">
-										<Typography variant="body1">{tokenBalance.balanceDisplay(6)}</Typography>
-									</Box>
-								</Grid>
-								<Grid item container justifyContent="flex-end" className={classes.balanceDisplayValue}>
-									<Typography variant="subtitle2" color="textSecondary">
-										{tokenBalance.balanceValueDisplay(uiState.currency, 6)}
-									</Typography>
-								</Grid>
-							</Grid>
+							<WalletTokenBalance key={tokenBalance.token.address} balance={tokenBalance} />
 						))}
 					</Grid>
 				</Grid>
