0d5e2366b86a04909ee4e110568230da37bd9eb8
elio <eliobricenov@gmail.com>
2022-03-02 18:27:46 -0400

    chore: add storybook config

---
+++ .storybook/preview.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { darkTheme } from '../src/config/ui/dark';
+import { ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline } from '@material-ui/core';
+
+export const parameters = {
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
+
+export const decorators = [
+	(Story) => (
+		<ThemeProvider theme={{ ...darkTheme, background: { ...darkTheme.background, default: '#fff' } }}>
+			<CssBaseline />
+			<Story />
+		</ThemeProvider>
+	),
+];
