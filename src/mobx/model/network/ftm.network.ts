893a947f7dc6975ca116da975ff3acd042eccfd1
jintao <hellojintao@protonmail.com>
2022-03-03 18:07:04 -0500

    chore: add oxd vault

--- src/mobx/model/network/ftm.network.ts
+++ src/mobx/model/network/ftm.network.ts
@@ -76,6 +76,16 @@ export const ftmVaults: BadgerVault[] = [
 			decimals: 18,
 		},
 	},
+	{
+		depositToken: {
+			address: FTM_DEPLOY.tokens['solidly.oxd-usdc'],
+			decimals: 18,
+		},
+		vaultToken: {
+			address: FTM_DEPLOY.sett_system.vaults['native.oxd-usdc'],
+			decimals: 18,
+		},
+	},
 ];
 
 const ftmTokens = ftmVaults.flatMap((vault) => [vault.depositToken, vault.vaultToken]);
