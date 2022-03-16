be0207c73a65510e20c280c13ea61c72b2c9eed8
Fatima Naz <fatima@Fatimas-MacBook-Pro.local>
2022-03-16 21:31:58 +0530

    fix: some liniting issue

--- src/components-v2/navbar/NavbarSocialContact.tsx
+++ src/components-v2/navbar/NavbarSocialContact.tsx
@@ -60,7 +60,7 @@ export const NavbarSocialContact = (): JSX.Element => {
 			</Grid>
 			<Grid item className={classes.governance}>
 				<Typography variant="helperText">
-					<Link color="inherit" href="/governance" rel="noopener" >
+					<Link color="inherit" href="/governance" rel="noopener">
 						GOVERNANCE
 					</Link>
 				</Typography>
