f461be13317e18480765b3514c18eaafd005ae32
elio <eliobricenov@gmail.com>
2022-03-21 03:11:13 -0400

    refactor: remove forced width

--- src/components-v2/landing/VaultLogo.tsx
+++ src/components-v2/landing/VaultLogo.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Vault } from '@badger-dao/sdk';
+import { getTokenIconPath } from '../../utils/componentHelpers';
 
 const logoWidth = 32;
-const logoHeight = 32;
 const overlapGapPercentage = 0.3; // we want the tokens to be overlapping by 30% of their width
+const spacingGap = -(logoWidth * overlapGapPercentage);
 
 const useStyles = makeStyles({
 	root: {
@@ -17,11 +18,11 @@ const useStyles = makeStyles({
 function getLogoStyles(index: number, totalLogos: number) {
 	return makeStyles({
 		position: {
-			height: logoHeight,
-			width: logoWidth,
+			// allow each logo file to have the width of up to three times the expected size
+			maxWidth: logoWidth * 2,
 			zIndex: totalLogos - index,
 			// we move the logos to the left except from the first logo
-			marginRight: index === 0 ? 0 : -(logoWidth * overlapGapPercentage),
+			marginRight: index === 0 ? 0 : spacingGap,
 		},
 	})().position;
 }
@@ -37,7 +38,7 @@ const VaultLogo = ({ tokens }: Props): JSX.Element => {
 			{tokens.map((token, index, totalTokens) => (
 				<img
 					key={`${token.symbol}_${index}`}
-					src={`/assets/icons/${token.symbol.toLowerCase()}.svg`}
+					src={getTokenIconPath(token)}
 					className={getLogoStyles(index, totalTokens.length)}
 					alt={`${token.symbol} logo`}
 				/>
