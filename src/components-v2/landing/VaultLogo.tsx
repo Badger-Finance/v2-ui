0f01b0bb0211625091dc778b15f8b7f6ab11c66e
elio <eliobricenov@gmail.com>
2022-03-22 14:08:23 -0400

    refactor: use generalized token logo component with fallback across the dapp

--- src/components-v2/landing/VaultLogo.tsx
+++ src/components-v2/landing/VaultLogo.tsx
@@ -1,7 +1,7 @@
-import React, { SyntheticEvent } from 'react';
+import React from 'react';
 import { makeStyles } from '@material-ui/core';
 import { Vault } from '@badger-dao/sdk';
-import { getTokenIconPath } from '../../utils/componentHelpers';
+import TokenLogo from '../TokenLogo';
 
 const logoWidth = 32;
 const overlapGapPercentage = 0.3; // we want the tokens to be overlapping by 30% of their width
@@ -33,21 +33,13 @@ interface Props {
 
 const VaultLogo = ({ tokens }: Props): JSX.Element => {
 	const classes = useStyles();
-
-	const handleError = ({ currentTarget }: SyntheticEvent<HTMLImageElement>) => {
-		currentTarget.src = '/assets/icons/token-logo-fallback.svg';
-		currentTarget.onerror = null;
-	};
-
 	return (
 		<div className={classes.root}>
 			{tokens.map((token, index, totalTokens) => (
-				<img
+				<TokenLogo
+					token={token}
 					key={`${token.symbol}_${index}`}
-					src={getTokenIconPath(token)}
 					className={getLogoStyles(index, totalTokens.length)}
-					alt={`${token.symbol} logo`}
-					onError={handleError}
 				/>
 			))}
 		</div>
--- src/components/IbBTC/OptionTokens.tsx
+++ src/components/IbBTC/OptionTokens.tsx
@@ -5,7 +5,7 @@ import { Typography, Button, Popper, Paper, List, ListItem } from '@material-ui/
 import { ArrowDropDown } from '@material-ui/icons';
 import { TokenBalance } from '../../mobx/model/tokens/token-balance';
 import { Token } from '@badger-dao/sdk';
-import { getTokenIconPath } from '../../utils/componentHelpers';
+import TokenLogo from '../../components-v2/TokenLogo';
 
 const useStyles = makeStyles((theme) => ({
 	noUnderline: {
@@ -109,7 +109,7 @@ export const OptionToken = ({ token }: TokenProps): JSX.Element => {
 	const classes = useStyles();
 	return (
 		<div className={classes.tokenContainer}>
-			<img className={classes.tokenIcon} src={getTokenIconPath(token)} alt={`${symbol} icon`} />
+			<TokenLogo token={token} className={classes.tokenIcon} />
 			<Typography variant="body1" component="div">
 				{symbol}
 			</Typography>
