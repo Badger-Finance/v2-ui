0469c0674b529f979cf32bed7adceeed3144ea7c
jintao <hellojintao@protonmail.com>
2022-03-26 12:44:17 -0400

    chore: add oxd vault advisory

--- src/components-v2/common/dialogs/LockingVaultAdivsory.tsx
+++ src/components-v2/common/dialogs/LockingVaultAdivsory.tsx
@@ -1,11 +1,17 @@
+import { VaultDTO } from '@badger-dao/sdk';
 import React from 'react';
 import AdvisoryLink from './AdvisoryLink';
 import GenericVaultAdvisory, { VaultAdvisoryBaseProps } from './GenericVaulAdvisory';
 
-const LockingVaultAdvisory = ({ accept }: VaultAdvisoryBaseProps): JSX.Element => {
+interface LockingVaultAdvisoryProps extends VaultAdvisoryBaseProps {
+	vault: VaultDTO;
+}
+
+const LockingVaultAdvisory = ({ vault, accept }: LockingVaultAdvisoryProps): JSX.Element => {
+	const depositToken = vault.underlyingToken;
 	return (
 		<GenericVaultAdvisory accept={accept}>
-			<p>This vault locks Convex in a staking contract that is 16 weeks long.</p>
+			<p>This vault locks {depositToken} in a staking contract that is 16 weeks long.</p>
 			<p>
 				The Total Withdrawable Amount in the vault may be lower than your balance depending on what is currently
 				available and will fluctuate with deposits and withdraws from the vault as well as locks and unlocks
@@ -15,6 +21,7 @@ const LockingVaultAdvisory = ({ accept }: VaultAdvisoryBaseProps): JSX.Element =
 				Any vault withdraws will have an opportunity to utilize the freshly deposited liquidity before it gets
 				locked on a first-come-first-serve basis.
 			</p>
+			{/* TODO: remove or update this page on a per locked token basis */}
 			<AdvisoryLink
 				href="https://docs.badger.com/badger-finance/sett-user-guides/blcvx-locked-convex"
 				linkText="Learn More"
--- src/tests/VaultAdvisory.test.tsx
+++ src/tests/VaultAdvisory.test.tsx
@@ -3,6 +3,7 @@ import '@testing-library/jest-dom';
 import { checkSnapshot } from './utils/snapshots';
 import { AdvisoryType } from 'mobx/model/vaults/advisory-type';
 import VaultAdvisory from 'components-v2/common/dialogs/VaultAdvisory';
+import { SAMPLE_VAULT } from './utils/samples';
 
 describe('VaultAdvisory', () => {
 	describe('Shows the proper vault advisory if one exists, or no advisory', () => {
@@ -13,7 +14,7 @@ describe('VaultAdvisory', () => {
 			[AdvisoryType.None],
 			['Invalid'], // purposeful bad enum case
 		])('Displays the expected %s vault advisory', (advisoryType: string) => {
-			checkSnapshot(<VaultAdvisory accept={() => {}} type={advisoryType as AdvisoryType} />);
+			checkSnapshot(<VaultAdvisory vault={SAMPLE_VAULT} accept={() => {}} type={advisoryType as AdvisoryType} />);
 		});
 	});
 });
