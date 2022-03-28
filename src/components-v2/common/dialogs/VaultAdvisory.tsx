ac3c1ad240d6c01bb797328ff234fb5f3859b954
elio <eliobricenov@gmail.com>
2022-03-28 10:49:43 -0400

    chore: add prop customization to locking vault advisory

--- src/components-v2/common/dialogs/VaultAdvisory.tsx
+++ src/components-v2/common/dialogs/VaultAdvisory.tsx
@@ -15,7 +15,17 @@ const VaultAdvisory = ({ vault, type, accept }: Props): JSX.Element | null => {
 	let advisory: JSX.Element | null;
 	switch (type) {
 		case AdvisoryType.ConvexLock:
-			advisory = <LockingVaultAdvisory vault={vault} accept={accept} />;
+			advisory = (
+				<LockingVaultAdvisory
+					accept={accept}
+					vault={vault}
+					lockingWeeks={16}
+					learnMoreLink="https://docs.badger.com/badger-finance/sett-user-guides/blcvx-locked-convex"
+				/>
+			);
+			break;
+		case AdvisoryType.OxDAOLock:
+			advisory = <LockingVaultAdvisory vault={vault} accept={accept} lockingWeeks={16} />;
 			break;
 		case AdvisoryType.Remuneration:
 			advisory = <RemunerationVaultAdvisory accept={accept} />;
