5d390a66dd09b3ec861950af82e82fc04ec5e7c1
elio <eliobricenov@gmail.com>
2022-03-28 10:45:08 -0400

    chore: add locked deposits store

---
+++ src/mobx/stores/LockedDepositsStore.ts
@@ -0,0 +1,78 @@
+import { RootStore } from '../RootStore';
+import { extendObservable } from 'mobx';
+import { Network } from '../model/network/network';
+import { NETWORKS_LOCKED_DEPOSITS_CONFIG } from '../../config/networks-locked-deposits';
+import { ERC20__factory, VoteLockedDeposit__factory } from '../../contracts';
+import { BigNumber } from 'bignumber.js';
+import { TokenBalance } from '../model/tokens/token-balance';
+import { LockedContractInfo } from '../model/locked-deposits/locked-contract-info';
+import { ethers } from 'ethers';
+
+type LockedDepositBalancesMap = Map<string, TokenBalance>;
+
+class LockedDepositsStore {
+	private readonly store: RootStore;
+	private networksLockedDeposits = new Map<Network['id'], LockedDepositBalancesMap>();
+
+	constructor(store: RootStore) {
+		this.store = store;
+
+		extendObservable(this, {
+			networksLockedDeposits: this.networksLockedDeposits,
+		});
+	}
+
+	getLockedDepositBalances(address: string): TokenBalance | undefined {
+		return this.networksLockedDeposits.get(this.store.network.network.id)?.get(ethers.utils.getAddress(address));
+	}
+
+	async loadLockedBalances(): Promise<void> {
+		const {
+			network: { network },
+			onboard: { wallet },
+		} = this.store;
+
+		if (!wallet?.provider) {
+			return;
+		}
+
+		const tokens = NETWORKS_LOCKED_DEPOSITS_CONFIG[network.id];
+		const balances = await Promise.all(tokens.map(this.getLockedDepositBalance));
+		this.networksLockedDeposits.set(network.id, new Map(balances.flat()));
+	}
+
+	private getLockedDepositBalance = async ({
+		vaultAddress,
+		lockingContractAddress,
+		underlyingTokenAddress,
+		strategyAddress,
+	}: LockedContractInfo): Promise<[string, TokenBalance][]> => {
+		const {
+			onboard: { wallet },
+		} = this.store;
+
+		if (!wallet?.provider) {
+			return [];
+		}
+
+		const provider = new ethers.providers.Web3Provider(wallet.provider, 'any');
+		const token = this.store.vaults.getToken(underlyingTokenAddress);
+		const voteLockedDepositContract = VoteLockedDeposit__factory.connect(lockingContractAddress, provider);
+		const tokenContract = ERC20__factory.connect(underlyingTokenAddress, provider);
+
+		const [vaultBalance, strategyBalance, totalCVXBalanceStrategy, lockedCVXBalanceStrategy] = await Promise.all([
+			await tokenContract.balanceOf(vaultAddress),
+			await tokenContract.balanceOf(strategyAddress),
+			await voteLockedDepositContract.lockedBalanceOf(vaultAddress),
+			await voteLockedDepositContract.balanceOf(vaultAddress),
+		]);
+
+		const balance = new BigNumber(
+			vaultBalance.add(strategyBalance).add(totalCVXBalanceStrategy).add(lockedCVXBalanceStrategy)._hex,
+		);
+
+		return [[ethers.utils.getAddress(underlyingTokenAddress), new TokenBalance(token, balance, balance)]];
+	};
+}
+
+export default LockedDepositsStore;
