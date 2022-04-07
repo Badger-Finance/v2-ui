a7127c68da2de5d5c7abcd2c222e4fafd79a5092
elio <eliobricenov@gmail.com>
2022-04-07 13:48:16 -0400

    chore: add deprecated vaults migration

--- src/config/deprecated-vaults-migrations.config.ts
+++ src/config/deprecated-vaults-migrations.config.ts
@@ -6,11 +6,24 @@ type Config = {
 	[networkId: Network['id']]: Record<string, string>;
 };
 
+const MAINNET_VAULTS = ETH_DEPLOY.sett_system.vaults;
+
 export const DEPRECATED_VAULTS_MIGRATIONS: Config = {
 	[NETWORK_IDS.ETH]: {
-		[ETH_DEPLOY.sett_system.vaults['native.badger']]: ETH_DEPLOY.sett_system.vaults['native.badgerCrv'],
-		[ETH_DEPLOY.sett_system.vaults['harvest.renCrv']]: ETH_DEPLOY.sett_system.vaults['native.ibbtcCrv'],
-		[ETH_DEPLOY.sett_system.vaults['native.uniDiggWbtc']]: ETH_DEPLOY.sett_system.vaults['native.ibbtcCrv'],
-		[ETH_DEPLOY.sett_system.vaults['native.tricryptoCrv']]: ETH_DEPLOY.sett_system.vaults['native.tricryptoCrv2'],
+		[MAINNET_VAULTS['native.pbtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.bbtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.sbtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.hbtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.obtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.tbtcCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.cvx']]: MAINNET_VAULTS['native.icvx'],
+		[MAINNET_VAULTS['native.sushiibBTCwBTC']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.uniBadgerWbtc']]: MAINNET_VAULTS['native.badgerCrv'],
+		[MAINNET_VAULTS['native.fPmBtcHBtc']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.imBtc']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['harvest.renCrv']]: MAINNET_VAULTS['native.ibbtcCrv'],
+		[MAINNET_VAULTS['native.badger']]: MAINNET_VAULTS['native.badgerCrv'],
+		[MAINNET_VAULTS['native.uniDiggWbtc']]: MAINNET_VAULTS['native.sushiDiggWbtc'],
+		[MAINNET_VAULTS['native.tricryptoCrv']]: MAINNET_VAULTS['native.tricryptoCrv2'],
 	},
 };
