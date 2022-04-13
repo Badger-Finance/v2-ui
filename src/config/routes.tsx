6fc0cd727bc1fde4685a9cd2dcb3969ca591b369
elio <eliobricenov@gmail.com>
2022-04-13 19:20:48 -0400

    chore: store and fetch filters in the url

--- src/config/routes.tsx
+++ src/config/routes.tsx
@@ -9,11 +9,32 @@ import CitadelEarlyBonding from 'pages/CitadelEarlyBonding';
 import { VaultDetail } from '../components-v2/vault-detail/VaultDetail';
 import { NotFound } from '../components-v2/common/NotFound';
 import Governance from 'components/Governance';
+import { Currency, Protocol, VaultBehavior, VaultState, VaultType } from '@badger-dao/sdk';
+import { VaultSortOrder } from '../mobx/model/ui/vaults-filters';
 
 const routes = {
 	home: new Route<RootStore, QueryParams>({
 		path: '/',
 		component: <Landing />,
+		onEnter: (route, params, store, queryParams) => {
+			const urlParams = new URLSearchParams(
+				Object.entries(queryParams).map(([key, value]) => [key, String(value)]),
+			);
+
+			store.vaults.vaultsFilters = {
+				hidePortfolioDust: Boolean(urlParams.get('hidePortfolioDust')),
+				showAPR: Boolean(urlParams.get('showAPR')),
+				onlyDeposits: Boolean(urlParams.get('onlyDeposits')),
+				onlyBoostedVaults: Boolean(urlParams.get('onlyBoostedVaults')),
+				currency: (urlParams.get('currency') as Currency) ?? store.uiState.currency,
+				sortOrder: (urlParams.get('sortOrder') as VaultSortOrder) ?? undefined,
+				search: (urlParams.get('search') as string) ?? undefined,
+				protocols: urlParams.getAll('protocols') as Protocol[],
+				types: urlParams.getAll('types') as VaultType[],
+				statuses: urlParams.getAll('statutes') as VaultState[],
+				behaviors: urlParams.getAll('behaviors') as VaultBehavior[],
+			};
+		},
 	}),
 	notFound: new Route<RootStore, QueryParams>({
 		path: '/not-found',
