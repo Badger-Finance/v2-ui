532aa14cd208db5304f0ab96a1013c503671a8cd
elio <eliobricenov@gmail.com>
2022-04-14 00:24:02 -0400

    chore: fix lint warnings

--- src/components-v2/NetworkURLManager/index.tsx
+++ src/components-v2/NetworkURLManager/index.tsx
@@ -4,7 +4,7 @@ import { StoreContext } from '../../mobx/store-context';
 import usePrevious from 'hooks/usePrevious';
 import { NETWORK_IDS, NETWORK_IDS_TO_NAMES } from '../../config/constants';
 
-const NetworkURLManager = (): null => {
+const NetworkURLManager: React.FC = ({ children }) => {
 	const { router, onboard } = useContext(StoreContext);
 	const urlChainId = router.queryParams?.chain;
 	const chainId = onboard.chainId;
@@ -18,15 +18,15 @@ const NetworkURLManager = (): null => {
 		if (chainId !== previousChainId && onboard.isActive()) {
 			router.queryParams = { ...router.queryParams, chain: NETWORK_IDS_TO_NAMES[chainId as NETWORK_IDS] };
 		}
-	}, [chainId, previousChainId, urlChainId, router.queryParams, onboard.isActive()]);
+	}, [chainId, previousChainId, urlChainId, router, onboard]);
 
 	useEffect(() => {
 		if (chainId && !urlChainId) {
 			router.queryParams = { ...router.queryParams, chain: NETWORK_IDS_TO_NAMES[chainId as NETWORK_IDS] };
 		}
-	}, [chainId, router.queryParams]);
+	}, [chainId, router, urlChainId]);
 
-	return null;
+	return <>{children}</>;
 };
 
 export default observer(NetworkURLManager);
