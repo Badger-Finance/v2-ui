d9cf2fffd58465285c0ba2145d63d747bb3cf84c
jintao <hellojintao@protonmail.com>
2022-06-17 13:08:06 -0400

    chore: update lint

--- src/mobx/stores/rewardsStore.ts
+++ src/mobx/stores/rewardsStore.ts
@@ -319,53 +319,47 @@ class RewardsStore {
 		return sendContractMethod(this.store, method, options, `Claim submitted.`, `Rewards claimed.`);
 	});
 
-
-	reduceTimeSinceLastCycle(time: number): string  {
-	const timestamp = time * 1000;
-	const timeSinceLastCycle = Math.abs(Date.now() - timestamp);
-	return (
-		Math.floor(timeSinceLastCycle / (60 * 60 * 1000)) +
-		'h ' +
-		Math.round(((timeSinceLastCycle % 86400000) % 3600000) / 60000) +
-		'm'
-	);
-};
-
-reduceClaims(
-	proof: RewardMerkleClaim,
-	claimedRewards: TreeClaimData,
-	claims?: boolean,
-): TokenBalance[] {
-	if (!proof.cumulativeAmounts) {
-		return [];
-	}
-	const { rewards, vaults } = this.store;
-	const claimableTokens = proof.cumulativeAmounts.length;
-	const tokenClaims = [];
-
-	const amounts = claimedRewards[1];
-	for (let i = 0; i < claimableTokens; i++) {
-		const token = proof.tokens[i];
-		const claimToken = vaults.getToken(token);
-		if (!claimToken) {
-			continue;
-		}
-		const claimed = new BigNumber(amounts[i]);
-		const earned = new BigNumber(proof.cumulativeAmounts[i]);
-		const amount = earned.minus(claimed).gt(0) ? earned.minus(claimed) : new BigNumber(0);
-		let claimable;
-		if (claims) {
-			claimable = rewards.balanceFromProof(token, amount.toFixed());
-		} else {
-			claimable = new TokenBalance(claimToken, amount, new BigNumber(0));
-		}
-
-		tokenClaims.push(claimable);
+	reduceTimeSinceLastCycle(time: number): string {
+		const timestamp = time * 1000;
+		const timeSinceLastCycle = Math.abs(Date.now() - timestamp);
+		return (
+			Math.floor(timeSinceLastCycle / (60 * 60 * 1000)) +
+			'h ' +
+			Math.round(((timeSinceLastCycle % 86400000) % 3600000) / 60000) +
+			'm'
+		);
 	}
 
-	return tokenClaims;
-};
+	reduceClaims(proof: RewardMerkleClaim, claimedRewards: TreeClaimData, claims?: boolean): TokenBalance[] {
+		if (!proof.cumulativeAmounts) {
+			return [];
+		}
+		const { rewards, vaults } = this.store;
+		const claimableTokens = proof.cumulativeAmounts.length;
+		const tokenClaims = [];
 
+		const amounts = claimedRewards[1];
+		for (let i = 0; i < claimableTokens; i++) {
+			const token = proof.tokens[i];
+			const claimToken = vaults.getToken(token);
+			if (!claimToken) {
+				continue;
+			}
+			const claimed = new BigNumber(amounts[i]);
+			const earned = new BigNumber(proof.cumulativeAmounts[i]);
+			const amount = earned.minus(claimed).gt(0) ? earned.minus(claimed) : new BigNumber(0);
+			let claimable;
+			if (claims) {
+				claimable = rewards.balanceFromProof(token, amount.toFixed());
+			} else {
+				claimable = new TokenBalance(claimToken, amount, new BigNumber(0));
+			}
+
+			tokenClaims.push(claimable);
+		}
+
+		return tokenClaims;
+	}
 }
 
 export default RewardsStore;
