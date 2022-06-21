48b2b27a303876d0f6f4123848ef2be13adb4912
elio <eliobricenov@gmail.com>
2022-06-21 13:34:17 -0400

    chore(optimizer): handle zero nonNative balance

--- src/components/Boost/OptimizerHeader.tsx
+++ src/components/Boost/OptimizerHeader.tsx
@@ -41,27 +41,27 @@ const useStakeRatioClasses = (currentStakeRatio: number, accountStakeRatio = 0) 
 };
 
 const useStyles = makeStyles((theme) => ({
-    header: {
-        justifyContent: 'space-between',
-        [theme.breakpoints.down(480)]: {
-            justifyContent: 'center',
-        },
+  header: {
+    justifyContent: 'space-between',
+    [theme.breakpoints.down(480)]: {
+      justifyContent: 'center',
     },
-    boostText: {
-        fontSize: theme.spacing(4),
-        fontWeight: 400,
+  },
+  boostText: {
+    fontSize: theme.spacing(4),
+    fontWeight: 400,
     },
-    boostValue: {
-        fontSize: theme.spacing(4),
-        marginLeft: theme.spacing(1),
-    },
-    invalidMultiplier: {
-        color: theme.palette.error.main,
-    },
-    boostSectionContainer: {
-        display: 'flex',
-        alignItems: 'center',
-    },
+  boostValue: {
+    fontSize: theme.spacing(4),
+    marginLeft: theme.spacing(1),
+  },
+  invalidMultiplier: {
+    color: theme.palette.error.main,
+  },
+  boostSectionContainer: {
+    display: 'flex',
+    alignItems: 'center',
+  },
 }));
 
 interface Props {
@@ -69,42 +69,56 @@ interface Props {
     onReset: () => void;
 }
 
-const OptimizerHeader = ({stakeRatio, onReset}: Props): JSX.Element => {
-    const {
-        user: {accountDetails},
-    } = useContext(StoreContext);
-    const classes = useStyles();
-    const theme = useTheme();
-    const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
-    const accountStakeRatio = accountDetails ? accountDetails.nativeBalance / accountDetails.nonNativeBalance : 0;
-    const boostClasses = useStakeRatioClasses(stakeRatio, accountStakeRatio)();
-    const currentBoost = calculateUserBoost(stakeRatio);
-    return (
-        <Grid container spacing={isMobile ? 2 : 0} className={classes.header} alignItems="center">
-            <Grid item className={classes.boostSectionContainer}>
-                <Typography display="inline" variant="h6" className={classes.boostText}>
-                    Boost:
-                    <span className={clsx(classes.boostValue, boostClasses.fontColor)}>{`${currentBoost}x`}</span>
-                </Typography>
-                <Tooltip
-                    enterTouchDelay={0}
-                    title={
-                        'This is a boost estimation at a point in time for the purpose of illustration only. This is a means to help you optimize your returns. Please refer to the Sett page for your specific returns.'
-                    }
-                    arrow
-                    placement="bottom"
-                    color="primary"
-                >
-                    <StyledInfoIcon/>
-                </Tooltip>
-            </Grid>
-            <Grid item>
-                <Button color="primary" variant="outlined" size="small" onClick={onReset}>
-                    Reset Calculations
-                </Button>
-            </Grid>
-        </Grid>
-    );
+const OptimizerHeader = ({ stakeRatio, onReset }: Props): JSX.Element => {
+  const {
+    user: { accountDetails },
+  } = useContext(StoreContext);
+  const classes = useStyles();
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down('sm'));
+  const accountStakeRatio = accountDetails
+    ? accountDetails.nativeBalance / accountDetails.nonNativeBalance
+    : 0;
+  const boostClasses = useStakeRatioClasses(stakeRatio, accountStakeRatio)();
+  const currentBoost = calculateUserBoost(stakeRatio);
+  return (
+    <Grid
+      container
+      spacing={isMobile ? 2 : 0}
+      className={classes.header}
+      alignItems="center"
+    >
+      <Grid item className={classes.boostSectionContainer}>
+        <Typography display="inline" variant="h6" className={classes.boostText}>
+          Boost:
+          <span className={clsx(classes.boostValue, boostClasses.fontColor)}>
+            {`${currentBoost}x`}
+          </span>
+        </Typography>
+        <Tooltip
+          enterTouchDelay={0}
+          title={
+            'This is a boost estimation at a point in time for the purpose of illustration only. This is a means to help you optimize your returns. Please refer to the Sett page for your specific returns.'
+          }
+          arrow
+          placement="bottom"
+          color="primary"
+        >
+          <StyledInfoIcon />
+        </Tooltip>
+      </Grid>
+      <Grid item>
+        <Button
+          color="primary"
+          variant="outlined"
+          size="small"
+          onClick={onReset}
+        >
+          Reset Calculations
+        </Button>
+      </Grid>
+    </Grid>
+  );
 };
 
 export default observer(OptimizerHeader);
