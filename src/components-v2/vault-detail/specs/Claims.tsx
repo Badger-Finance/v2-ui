1b1d718e38c1b90f66bacb67aa93dd2980f9c6a3
Elio Briceño <eliobricenov@gmail.com>
2022-06-30 18:44:57 -0400

    chore: remove vault detail link

--- src/components-v2/vault-detail/specs/Claims.tsx
+++ src/components-v2/vault-detail/specs/Claims.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Grid, Link, Typography } from '@material-ui/core';
+import { Grid, Typography } from '@material-ui/core';
 import { observer } from 'mobx-react-lite';
 import { makeStyles } from '@material-ui/core/styles';
 import clsx from 'clsx';
@@ -8,7 +8,6 @@ import { StyledDivider } from '../styled';
 import { ESTIMATED_REWARDS_FREQUENCY } from '../../../config/constants';
 import { StoreContext } from '../../../mobx/store-context';
 import { calculateDelaySeverity, calculateDifferenceInHoursFromCycle } from '../utils';
-import ChevronRightIcon from '@material-ui/icons/ChevronRight';
 import BveCvxFrequencyInfo from '../../BveCvxFrequencyInfo';
 
 const useStyles = makeStyles((theme) => ({
@@ -88,17 +87,21 @@ export const Claims = observer((): JSX.Element => {
 					? 'This Sett’s rewards are currently taking longer than usual.'
 					: 'This Sett’s rewards are currently being processed.'}
 			</Typography>
-			<Link
-				className={classes.infoLink}
-				href="https://badger.wiki/setts#b52fee58e850405abd9701068fd93f37"
-				target="_blank"
-				rel="noreferrer"
-			>
-				<div className={classes.linkContent}>
-					<span>See more</span>
-					<ChevronRightIcon className={classes.linkIcon} />
-				</div>
-			</Link>
+			{/*TODO: Currently in the badger docs files the vaults slugs are different from the app.
+			   Re-enable this when the badger docs are updated with the same slugs so we can just do
+			   href=docs.badger/{vault.slug}
+			 */}
+			{/*<Link*/}
+			{/*	className={classes.infoLink}*/}
+			{/*	href="https://badger.wiki/setts#b52fee58e850405abd9701068fd93f37"*/}
+			{/*	target="_blank"*/}
+			{/*	rel="noreferrer"*/}
+			{/*>*/}
+			{/*	<div className={classes.linkContent}>*/}
+			{/*		<span>See more</span>*/}
+			{/*		<ChevronRightIcon className={classes.linkIcon} />*/}
+			{/*	</div>*/}
+			{/*</Link>*/}
 			<BveCvxFrequencyInfo open={infoDialogOpen} onClose={() => setInfoDialogOpen(false)} />
 		</Grid>
 	);
