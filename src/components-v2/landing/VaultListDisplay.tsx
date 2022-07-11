a3769ebae16bd57f2a3f8907589daac892fc52c7
jintao <hellojintao@protonmail.com>
2022-07-11 12:07:01 -0400

    chore: remove logging

--- src/components-v2/landing/VaultListDisplay.tsx
+++ src/components-v2/landing/VaultListDisplay.tsx
@@ -43,7 +43,6 @@ const VaultListDisplay = observer(() => {
     );
   }
 
-  console.log(vaultOrder);
   const settListItems = vaultOrder.flatMap((vault) => {
     const hasNoBalance = user.getBalance(vault.vaultToken).tokenBalance.eq(0);
 
