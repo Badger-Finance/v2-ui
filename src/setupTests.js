e6996d63138b0b630644368fbc49123677c83a76
Elio Briceño <eliobricenov@gmail.com>
2022-07-15 18:04:16 -0400

    chore: remove generalized api mocks

--- src/setupTests.js
+++ src/setupTests.js
@@ -1,20 +1,9 @@
 // See https://github.com/facebook/jest/issues/9983
 import fetchMock from 'jest-fetch-mock';
 
-// import { TextDecoder, TextEncoder } from 'util';
-import { setupMockAPI } from './tests/utils/setup';
-
-// Object.defineProperty(global, 'crypto', {
-//   value: { getRandomValues: (arr) => crypto.randomBytes(arr.length) },
-// });
-
 jest.mock('web3modal');
 
-// global.TextEncoder = TextEncoder;
-// global.TextDecoder = TextDecoder;
-
 fetchMock.enableMocks();
-setupMockAPI();
 
 export default function () {
   return null;
