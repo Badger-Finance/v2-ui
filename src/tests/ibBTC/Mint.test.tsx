536a1189144636751f8bbb3a33e5164bc6aee07a
Elio Briceño <eliobricenov@gmail.com>
2022-07-25 16:40:44 -0400

    tests(mint): update test

--- src/tests/ibBTC/Mint.test.tsx
+++ src/tests/ibBTC/Mint.test.tsx
@@ -3,151 +3,25 @@ import '@testing-library/jest-dom';
 
 import { ibBTCService, TransactionStatus } from '@badger-dao/sdk';
 import { cleanup, fireEvent, within } from '@testing-library/react';
-import { BigNumber } from 'ethers';
-import { parseEther, parseUnits } from 'ethers/lib/utils';
+import { parseEther } from 'ethers/lib/utils';
 import React from 'react';
 import { ToastContainer } from 'react-toastify';
 
 import { Mint } from '../../components/IbBTC/Mint';
-import { TokenBalances } from '../../mobx/model/account/user-balances';
 import { TokenBalance } from '../../mobx/model/tokens/token-balance';
 import store from '../../mobx/stores/RootStore';
 import { StoreProvider } from '../../mobx/stores/store-context';
 import { customRender, screen } from '../Utils';
-import { SAMPLE_IBBTC_TOKEN_BALANCE } from '../utils/samples';
-
-const tokenBalances: TokenBalances = {
-  '0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi renBTC/wBTC',
-      symbol: 'bcrvRenWBTC',
-      decimals: 18,
-      address: '0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545',
-    },
-    parseUnits('5', 18),
-    23972.80210514462,
-  ),
-  '0x6def55d2e18486b9ddfaa075bc4e4ee0b28c1545': new TokenBalance(
-    {
-      name: 'bCurve.fi: renCrv Token',
-      symbol: 'bcrvRenBTC',
-      decimals: 18,
-      address: '0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545',
-    },
-    parseUnits('5', 18),
-    23972.80210514462,
-  ),
-  '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D': new TokenBalance(
-    {
-      name: 'Ren Protocol BTC',
-      symbol: 'renBTC',
-      decimals: 8,
-      address: '0xEB4C2781e4ebA804CE9a9803C67d0893436bB27D',
-    },
-    parseUnits('10', 8),
-    23972.80210514462,
-  ),
-  '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599': new TokenBalance(
-    {
-      name: 'WBTC',
-      symbol: 'wbtc',
-      decimals: 8,
-      address: '0x2260FAC5E5542a773Aa44fBCfeDf7C193bc2C599',
-    },
-    parseUnits('0', 8),
-    23972.80210514462,
-  ),
-  '0xd04c48A53c111300aD41190D63681ed3dAd998eC': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi renBTC/wBTC/sBTC',
-      symbol: 'bcrvRenWSBTC',
-      decimals: 18,
-      address: '0xd04c48A53c111300aD41190D63681ed3dAd998eC',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0xb9D076fDe463dbc9f915E5392F807315Bf940334': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi tBTC/sbtcCrv',
-      symbol: 'btbtc/sbtcCrv',
-      decimals: 18,
-      address: '0xb9D076fDe463dbc9f915E5392F807315Bf940334',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0x4b92d19c11435614CD49Af1b589001b7c08cD4D5': new TokenBalance(
-    {
-      name: 'Badger WBTC yVault',
-      symbol: 'byvWBTC',
-      decimals: 8,
-      address: '0x4b92d19c11435614CD49Af1b589001b7c08cD4D5',
-    },
-    parseUnits('0', 8),
-    23972.80210514462,
-  ),
-  '0x8c76970747afd5398e958bDfadA4cf0B9FcA16c4': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi hBTC/wBTC',
-      symbol: 'bhCRV',
-      decimals: 18,
-      address: '0x8c76970747afd5398e958bDfadA4cf0B9FcA16c4',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0x5Dce29e92b1b939F8E8C60DcF15BDE82A85be4a9': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi bBTC/sbtcCRV',
-      symbol: 'bbBTC/sbtcCRV',
-      decimals: 18,
-      address: '0x5Dce29e92b1b939F8E8C60DcF15BDE82A85be4a9',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0xf349c0faA80fC1870306Ac093f75934078e28991': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi oBTC/sbtcCRV',
-      symbol: 'boBTC/sbtcCRV',
-      decimals: 18,
-      address: '0xf349c0faA80fC1870306Ac093f75934078e28991',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0x55912D0Cf83B75c492E761932ABc4DB4a5CB1b17': new TokenBalance(
-    {
-      name: 'Badger Sett Curve.fi pBTC/sbtcCRV',
-      symbol: 'bpBTC/sbtcCRV',
-      decimals: 18,
-      address: '0x55912D0Cf83B75c492E761932ABc4DB4a5CB1b17',
-    },
-    parseUnits('0', 18),
-    23972.80210514462,
-  ),
-  '0xc4E15973E6fF2A35cC804c2CF9D2a1b817a8b40F': new TokenBalance(
-    {
-      name: 'ibBTC',
-      symbol: 'ibBTC',
-      decimals: 18,
-      address: '0xc4E15973E6fF2A35cC804c2CF9D2a1b817a8b40F',
-    },
-    BigNumber.from('10000000000000000000'),
-    12.012381,
-  ),
-};
+import { SAMPLE_IBBTC_TOKEN_BALANCE, SAMPLE_IBBTC_USER_BALANCES } from '../utils/samples';
 
 jest.useFakeTimers();
 
 describe('ibBTC Mint', () => {
   beforeEach(() => {
     store.wallet.address = '0x1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a1a';
-    store.user.balances = tokenBalances;
+    store.user.balances = SAMPLE_IBBTC_USER_BALANCES;
     store.ibBTCStore.mintFeePercent = 0;
     store.ibBTCStore.redeemFeePercent = 0;
-    store.user.balances = tokenBalances;
 
     store.ibBTCStore.mintRates = {
       '0x6dEf55d2e18486B9dDfaA075bc4e4EE0B28c1545': '1.024385',
@@ -181,7 +55,7 @@ describe('ibBTC Mint', () => {
         <Mint />
       </StoreProvider>,
     );
-    expect(screen.getByText('Balance: 5.000000')).toBeInTheDocument();
+    expect(screen.getByText('Balance: 5.00000000')).toBeInTheDocument();
     expect(screen.getByText('Balance: 10.000000')).toBeInTheDocument();
   });
 
