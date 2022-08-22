3e863347dae79f6cf406ceedb2789588815bac59
mrbasado <mrbasado@protonmail.com>
2022-08-22 17:13:15 -0500

    fix boost test and some snapshots

--- src/tests/unit/componentHelpers.test.ts
+++ src/tests/unit/componentHelpers.test.ts
@@ -183,7 +183,6 @@ describe('Component Helpers', () => {
               maxApr: 5.829144717979947,
             },
           ],
-          state: VaultState.Open,
         },
       ],
     ])('getUserVaultBoost(%s, %d) returns %p', (_testCaseName, boost, calculatedBoost, vault) => {
