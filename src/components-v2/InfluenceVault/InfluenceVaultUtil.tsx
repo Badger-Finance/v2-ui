c52296a75b03e3ce9845d15f08274c4277810e82
mrbasado <mrbasado@protonmail.com>
2022-08-23 10:26:43 -0500

    turn on graviAura graph and remove first 4 rounds

--- src/components-v2/InfluenceVault/InfluenceVaultUtil.tsx
+++ src/components-v2/InfluenceVault/InfluenceVaultUtil.tsx
@@ -122,11 +122,11 @@ export const vaults: InfluenceVaultConfig[] = [
     influenceVaultToken: mainnetDeploy.sett_system.vaults['native.graviaura'], // graviAura
     poolToken: '',
     vaultToken: mainnetDeploy.tokens['aura'], //Aura
-    roundStart: 1658545600,
-    enableChart: false,
+    roundStart: 1655082061,
+    enableChart: true,
     sources: [mainnetDeploy.sett_system.vaults['native.graviaura'], mainnetDeploy.tokens['badger']],
     scheduleRoundCutoff: 0,
-    chartInitialSlice: 0,
+    chartInitialSlice: 4,
     rewardFrequencies: [
       {
         name: 'graviAURA, BADGER',
