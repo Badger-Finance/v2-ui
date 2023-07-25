ec99b9148e3caf4088104e6c17aff577ebbd2e73
Anil Kumar Rai <anilrai@outlook.com>
2023-07-25 20:01:19 +0530

    fix: console warning after network switch

--- src/mobx/stores/WalletStore.ts
+++ src/mobx/stores/WalletStore.ts
@@ -72,14 +72,7 @@ export class WalletStore {
 
   /* eslint-disable-next-line  @typescript-eslint/no-explicit-any */
   private getLibrary(provider: any): Web3Provider {
-    const library = new Web3Provider(
-      provider,
-      typeof provider.chainId === 'number'
-        ? provider.chainId
-        : typeof provider.chainId === 'string'
-        ? parseInt(provider.chainId)
-        : 'any',
-    );
+    const library = new Web3Provider(provider, 'any');
     library.pollingInterval = 15000;
     return library;
   }
